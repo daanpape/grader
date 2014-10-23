b6adfce440ecb0a4a157ef10cb9ecfc2b8698d33
Matthieu Calie <matthieu.calie@student.howest.be>
2014-10-23 15:40:34 +0200

    favicon added :)

--- templates/assess.php
+++ templates/assess.php
@@ -16,7 +16,7 @@ $location = "assess";
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
+    <link rel="icon" href="/img/favicon.ico">
 
     <title data-bind="text: title"></title>
 
--- templates/home.php
+++ templates/home.php
@@ -10,7 +10,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="description" content="">
 		<meta name="author" content="">
-		<link rel="icon" href="favicon.ico">
+		<link rel="icon" href="/img/favicon.ico">
 
 		<title data-bind="text: title"></title>
 
