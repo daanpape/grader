363277bb906f9ec89a3f85a5366adf1c91b5192c
Matthieu Calie <matthieu.calie@student.howest.be>
2014-11-04 21:22:35 +0100

    typo in assessment page

--- js/assessViewModel.js
+++ js/assessViewModel.js
@@ -32,7 +32,7 @@ function pageViewModel(gvm) {
 
 function loadTablePage(pagenr)
 {
-    $.getJSON('/api/projectts/page/' + pagenr, function(data){
+    $.getJSON('/api/projects/page/' + pagenr, function(data){
 
         /* Clear current table page */
         viewModel.clearTable();
