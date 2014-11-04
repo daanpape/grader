fdf65e43f10b5597d1c793b530eba3be33bfb0ad
Matthieu Calie <matthieu.calie@student.howest.be>
2014-11-04 21:40:41 +0100

    missing line

--- js/coursesViewModel.js
+++ js/coursesViewModel.js
@@ -1,8 +1,9 @@
-// View model for the projects page
+// View model for the courses page
 function pageViewModel(gvm) {
     // Page specific i18n bindings
     gvm.title = ko.computed(function(){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("CoursesTitle");}, gvm);
     gvm.pageHeader = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("CoursesHeader");}, gvm);
+    gvm.projectname = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("ProjectName");}, gvm);
 }
 
 function initPage() {
