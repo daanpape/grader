5744abb62ad0a28aa14a5d5d761880a81346e20c
Daan Pape <daan@dptechnics.com>
2014-11-06 13:33:42 +0100

    fixed i18n null on home bug

--- js/homeViewModel.js
+++ js/homeViewModel.js
@@ -3,6 +3,7 @@ function pageViewModel(gvm) {
     // Page specific i18n bindings
     gvm.title = ko.computed(function (){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("HomeTitle");}, gvm);
     gvm.projectname = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("ProjectName");}, gvm);
+    gvm.homeManual = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("HomeManual")}, gvm);
 
 }
 
