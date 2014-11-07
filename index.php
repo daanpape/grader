1fdc11e2cc79b97dd1310a44bc14cd3cdb1c698f
Matthieu Calie <matthieu.calie@student.howest.be>
2014-11-07 16:11:17 +0100

    courses

--- index.php
+++ index.php
@@ -147,7 +147,7 @@ $app->get('/api/trainings/:locationId', function ($locationId) use ($app) {
     echo json_encode($pagedata);
 });
 
-/*$app->get('/api/courses/:trainingId', function ($trainingId) use ($app) {
+$app->get('/api/courses/:trainingId', function ($trainingId) use ($app) {
     // Use json headers
     $response = $app->response();
     $response->header('Content-Type', 'application/json');
@@ -156,7 +156,7 @@ $app->get('/api/trainings/:locationId', function ($locationId) use ($app) {
     $pagedata = GraderAPI::getCoursesByTraining($trainingId);
 
     echo json_encode($pagedata);
-});*/
+});
 
 $app->get('/api/courses/:locationId/:trainingId', function($locationId, $trainingId) use ($app) {
     $response = $app->response();
@@ -168,15 +168,15 @@ $app->get('/api/courses/:locationId/:trainingId', function($locationId, $trainin
     echo json_encode($pagedata);
 });
 
-$app->get('/api/courses/:locationId', function($locationId) use ($app) {
-    $response = $app->response();
-    $response->header('Content-Type', 'application/json');
-
-    // Get all courses by the trainingsid
-    $pagedata = GraderAPI::getLocationsTrainingsAndCoursesByLocation($locationId);
-
-    echo json_encode($pagedata);
-});
+//$app->get('/api/courses/:locationId', function($locationId) use ($app) {
+//    $response = $app->response();
+//    $response->header('Content-Type', 'application/json');
+//
+//    // Get all courses by the trainingsid
+//    $pagedata = GraderAPI::getLocationsTrainingsAndCoursesByLocation($locationId);
+//
+//    echo json_encode($pagedata);
+//});
 
 // API PUT routes
 $app->put('/api/project/:id', function($id) use ($app){
