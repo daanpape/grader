853076835b35b5b27d183872d216ad1362fd52d0
Matthieu Calie <matthieu.calie@student.howest.be>
2014-11-07 16:05:04 +0100

    courses

--- api.php
+++ api.php
@@ -109,7 +109,7 @@ class GraderAPI {
     }
 
     public static function test($locationId) {
-        return ClassDAO::getTrainingsByLocation($locationId);
+        return ClassDAO::getTrainingsByLocation($locationId[1].id);
 
     }
 }
