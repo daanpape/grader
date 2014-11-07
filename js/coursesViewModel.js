118b57dd86a3b2593edfb1a223452158bc69a2d9
Matthieu Calie <matthieu.calie@student.howest.be>
2014-11-07 17:20:08 +0100

    courses

--- js/coursesViewModel.js
+++ js/coursesViewModel.js
@@ -5,23 +5,23 @@ function pageViewModel(gvm) {
     gvm.pageHeader = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("CoursesHeader");}, gvm);
     gvm.projectname = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("ProjectName");}, gvm);
 
-    gvm.availableLocations = ko.observableArray([]);
-    gvm.availableTrainings = ko.observableArray([]);
-    gvm.availableCourses = ko.observableArray([]);
+    gvm.availableLocations;
+    gvm.availableTrainings;
+    gvm.availableCourses;
 
-    gvm.addAvailableLocations = function(id, name) {
-        var selectObject = {id: id, locationName: name};
-        gvm.availableLocations.push(selectObject);
-    }
-    gvm.addAvailableTrainings = function(id, name) {
-        var selectObject = {id: id, trainingName: name};
-        gvm.availableTrainings.push(selectObject);
-    }
-
-    gvm.addAvailableCourses = function(id, name) {
-        var selectObject = {id: id, courseName: name};
-        gvm.availableCourses.push(selectObject);
-    }
+//    gvm.addAvailableLocations = function(id, name) {
+//        var selectObject = {id: id, locationName: name};
+//        gvm.availableLocations.push(selectObject);
+//    }
+//    gvm.addAvailableTrainings = function(id, name) {
+//        var selectObject = {id: id, trainingName: name};
+//        gvm.availableTrainings.push(selectObject);
+//    }
+//
+//    gvm.addAvailableCourses = function(id, name) {
+//        var selectObject = {id: id, courseName: name};
+//        gvm.availableCourses.push(selectObject);
+//    }
 
     gvm.clearAll = function() {
         gvm.availableLocations.removeAll();
@@ -71,12 +71,7 @@ function pageViewModel(gvm) {
 //}
 
 function loadLocations() {
-    $.getJSON('/api/locations', function(data) {
-        $.each(data, function(i, item) {
-            viewModel.addAvailableLocations(item.id, item.name);
-        });
-    });
-    $("#location").unbind("change");
+    viewModel.availableLocations = ko.mapping.fromJSON("/api/locations")
 }
 
 function loadTraining($id) {
