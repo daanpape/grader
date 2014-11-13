e1b1933a1a9f22a122a3ff5594a8bec78b181242
Daan Pape <daan@dptechnics.com>
2014-11-13 11:13:05 +0100

    course selection

--- templates/courses.php
+++ templates/courses.php
@@ -32,7 +32,7 @@ $location = "courses";
                 <span class="caret"></span>
             </button>
             <ul class="dropdown-menu" role="menu" aria-labelledby="locations" data-bind="foreach: availableLocations">
-                <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><span data-bind="text: name, attr: {data-id: id}"></span></a> </li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><span data-bind="text: name, attr: {'data-id': id}"></span></a> </li>
             </ul>
         </div>
 
