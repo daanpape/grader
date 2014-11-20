d1afd883b7979fcdae0d572171512e7b9cc8cec7
Daan Pape <daan@dptechnics.com>
2014-11-20 11:59:45 +0100

    update home

--- templates/home.php
+++ templates/home.php
@@ -1,33 +1,38 @@
 <?php
-	// Page initialisation
-	$location = "home";
+// Page initialisation
+$location = "home";
 ?>
 <!DOCTYPE html>
 <html lang="nl" id="htmldoc">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="description" content="">
-		<meta name="author" content="">
-		<link rel="icon" href="/img/favicon.ico">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="/img/favicon.ico">
 
-		<title data-bind="text: title"></title>
+        <title data-bind="text: title"></title>
 
-		<?php include_once('hddepends.php') ?>
-	</head>
+        <?php include_once('hddepends.php') ?>
+    </head>
 
-	<body>
-		<?php include_once('menu.php') ?>
+    <body>
+        <?php include_once('menu.php') ?>
 
-		<div class="container">
-			<div class="starter-template" data-bind="html: homeManual">
-				<h1>Assessment tool</h1>
-				<p class="lead">This HoWest Assessment tool is used to grade students.<br/>Please read this short manual
-				before using the service.</p>
-			</div>
-		</div>
+        <!-- Header container -->
+        <div class="container">
+            <h1 class="page-header" data-bind="text: app">Projects</h1>
+        </div>
+        
+        <!-- Content container -->
+        <div class="container">
+            <div class="starter-template" data-bind="html: homeManual">
+                <p class="lead">This HoWest Assessment tool is used to grade students.<br/>Please read this short manual
+                    before using the service.</p>
+            </div>
+        </div>
 
-    <?php include_once('jsdepends.php') ?>
-  </body>
+        <?php include_once('jsdepends.php') ?>
+    </body>
 </html>
