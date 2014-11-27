51b0071255fae5d76d393de97f40d61c9f201755
Matthieu <matthieu.calie@gmail.com>
2014-11-27 22:36:04 +0100

    projectpage

--- templates/assess.php
+++ templates/assess.php
@@ -28,11 +28,48 @@ $location = "assess";
 
 <!-- Header container -->
 <div class="container">
-    <h1 class="page-header" data-bind="text: pageHeader">Projecttypes</h1>
+    <h1 class="page-header" data-bind="text: pageHeader">Assess</h1>
+</div>
+<!-- Content container -->
+<div class="container">
+    <div class="big-info"><span data-bind="text: selectCourse">Select course</span>:</div>
+    <div class="row">
+        <div class="dropdown col-md-4">
+            <button class="btn btn-wide btn-default btn-location dropdown-toggle" type="button" id="locations" data-toggle="dropdown" aria-expanded="true">
+                <span class="text-center">Locations</span>
+                <span class="pull-right caret-down caret"></span>
+            </button>
+            <ul class="dropdown-menu dropdown-location ul-wide" role="menu" aria-labelledby="locations" data-bind="foreach: availableLocations" id="testcliker">
+                <li class="li-wide" role="presentation"><a role="menuitem" tabindex="-1" href="#" data-bind="attr:{'id': 'locbtn-' + id}"><span data-bind="text: name"></span></a> </li>
+            </ul>
+        </div>
+
+        <div class="dropdown col-md-4">
+            <button class="btn btn-wide btn-default btn-training dropdown-toggle" type="button" id="trainings" data-toggle="dropdown" aria-expanded="true">
+                <span class="text-center">Trainings</span>
+                <span class="pull-right caret-down caret"></span>
+            </button>
+            <ul class="dropdown-menu dropdown-training ul-wide" role="menu" aria-labelledby="trainings" data-bind="foreach: availableTrainings">
+                <li class="li-wide" role="presentation"><a role="menuitem" tabindex="-1" href="#" data-bind="attr:{'id': 'trainingbtn-' + id}"><span data-bind="text: name"></span></a></li>
+            </ul>
+        </div>
+
+        <div class="dropdown col-md-4">
+            <button class="btn btn-wide btn-default btn-course dropdown-toggle" type="button" id="availableCourses" data-toggle="dropdown" aria-expanded="true">
+                <span class="text-center">Courses</span>
+                <span class="pull-right caret-down caret"></span>
+            </button>
+            <ul class="dropdown-menu dropdown-course ul-wide" role="menu" aria-labelledby="availableCourses" data-bind="foreach: availableCourses">
+                <li class="li-wide" role="presentation"><a role="menuitem" tabindex="-1" href="#" data-bind="attr:{'id': 'coursebtn-' + id}"><span data-bind="text: name"></span></a> </li>
+            </ul>
+
+        </div>
+    </div>
 </div>
 
 <!-- Content container -->
 <div class="container">
+    <div class="big-info"><span data-bind="text: foundProjects">Found projects</span>:</div>
     <table class="table table-striped">
         <thead>
         <tr>
@@ -48,7 +85,9 @@ $location = "assess";
             <td data-bind="text: tname">--</td>
             <td data-bind="text: tdesc">--</td>
             <td>
-                buttons...
+                <span class="glyphicon glyphicon-pencil glyphicon-btn" data-bind="attr:{'id': 'editbtn-' + tid}"></span>
+                <span class="glyphicon glyphicon-trash glyphicon-btn" data-bind="attr:{'id': 'removebtn-' + tid}"></span>
+                <a data-bind="attr:{'href': '/project/' + tid}"><span class="glyphicon glyphicon-plus glyphicon-btn" data-bind="attr:{'id': 'managebtn-' + tid}"></span></a>
             </td>
         </tr>
         </tbody>
@@ -59,20 +98,18 @@ $location = "assess";
 <div class="container">
     <!-- Pagination -->
     <ul class="pagination float_left">
-        <li class="disabled"><a href="#">&laquo;</a></li>
-        <li><a href="#">1</a></li>
-        <li class="active"><a href="#">2</a></li>
-        <li><a href="#">3</a></li>
-        <li><a href="#">4</a></li>
-        <li><a href="#">5</a></li>
-        <li><a href="#">&raquo;</a></li>
+        <li id="pager-prev-btn"><a href="#" >&laquo;</a></li>
+        <li class="pager-nr-btn"><a href="#" >1</a></li>
+        <li class="pager-nr-btn"><a href="#" >2</a></li>
+        <li class="pager-nr-btn"><a href="#">3</a></li>
+        <li class="pager-nr-btn"><a href="#">4</a></li>
+        <li class="pager-nr-btn"><a href="#">5</a></li>
+        <li id="pager-next-btn"><a href="#" >&raquo;</a></li>
     </ul>
 
-    <!--
     <button type="button" class="btn btn-default pagination-button" id="addProjectTypeBtn">
         <span class="glyphicon glyphicon-plus"></span> <span data-bind="text: addBtn"></span>
     </button>
-    -->
 </div>
 
 <?php include_once('jsdepends.php') ?>
