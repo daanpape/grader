7dbe210e480079d0c82d4a64ec60d397f2fbcb85
Daan Pape <daan@dptechnics.com>
2014-12-04 14:43:31 +0100

    updated project structure

--- js/projectViewModel.js
+++ js/projectViewModel.js
@@ -1,4 +1,4 @@
-function Competence(code, name, weight, subcompetences) {
+function Competence(viewmodel, code, name, weight, subcompetences) {
     return {
         code: ko.observable(code),
         name: ko.observable(name),
@@ -7,11 +7,19 @@ function Competence(code, name, weight, subcompetences) {
         
         addSubCompetence: function() {
             this.subcompetences.push(new SubCompetence());
+        },
+        
+        removeThis: function() {
+            viewmodel.removeCompetence(this);
+        },
+        
+        removeSubCompetence: function(subCompetence) {
+            this.subcompetences.remove(subCompetence);
         }
     };
 }
 
-function SubCompetence(code, name, weight, indicators) {
+function SubCompetence(parent, code, name, weight, indicators) {
     return {
         code: ko.observable(code),
         name: ko.observable(name),
@@ -20,14 +28,26 @@ function SubCompetence(code, name, weight, indicators) {
         
         addIndicator: function() {
             this.indicators.push(new Indicator());
+        },
+        
+        removeThis: function() {
+            parent.removeSubCompetence(this);
+        },
+        
+        removeIndicator: function(indicator) {
+            this.indicators.remove(indicator);
         }
     };
 }
 
-function Indicator(name, description) {
+function Indicator(parent, name, description) {
     return {
         name: ko.observable(name),
-        description : ko.observable(description)
+        description : ko.observable(description),
+        
+        removeThis: function() {
+            parent.removeIndicator(this);
+        }
     };
 }
 
@@ -57,154 +77,11 @@ function pageViewModel(gvm) {
     gvm.addCompetence = function() {
         gvm.competences.push(new Competence());
     };
-}
-
-function addCompetence() {
-    var competencePanelWrapper = document.createElement('div');
-    var competencePanel = document.createElement('div');
-    var competencePanelHeading = document.createElement('div');
-    var competencePanelBody = document.createElement('div');
-    var competencePanelFooter = document.createElement('div');
-    var competenceCode = document.createElement('input');
-    var competenceName = document.createElement('input');
-    var weightSpan = document.createElement('span');
-    var subcompetenceButton = document.createElement('button');
-    var removeCompetenceButton = document.createElement('button');
-
-
-    competenceCode.type = 'text';
-    competenceCode.placeholder = "Competence-Code";
-    $(competenceCode).addClass("form-control form-next");
-
-    competenceName.type = 'text';
-    competenceName.placeholder = "Name of the competence";
-    $(competenceName).addClass("form-control form-next");
-
-    $(subcompetenceButton).addClass("btn");
-    $(subcompetenceButton).text("Add a subcompetence");
-    $(subcompetenceButton).val(viewModel.competences);
-    $(subcompetenceButton).on('click', function() {
-        addSubCompetence($(subcompetenceButton).val());
-    });
-
-    $(removeCompetenceButton).addClass("btn pull-right");
-    $(removeCompetenceButton).text("Remove this competence");
-    $(removeCompetenceButton).val(viewModel.competences);
-    $(removeCompetenceButton).on('click', function() {
-        $(this).parent().parent().remove();
-        viewModel.competences--;
-    });
-
-    $(competencePanelWrapper).addClass("col-md-12 compPanel panel-" + $(subcompetenceButton).val());
-    $(competencePanel).addClass("panel panel-default");
-    $(competencePanelHeading).addClass("panel-heading comp-" + $(subcompetenceButton).val());
-    $(competencePanelBody).addClass("panel-body comp-"  + $(subcompetenceButton).val());
-    $(competencePanelFooter).addClass("panel-footer");
-
-    if(!$(".compPanel")[0]) {
-        $("#top-col").after(competencePanelWrapper);
-    } else {
-        $(".compPanel:last").after(competencePanelWrapper);
+    
+    gvm.removeCompetence = function(competence) {
+        gvm.competences.remove(competence);
     }
-    ++viewModel.competences;
-
-    $(weightSpan).text("Current Percentage: " + 100 / viewModel.competences);
-
-
-    $(competencePanelWrapper).append(competencePanel);
-    $(competencePanel).append(competencePanelHeading);
-    $(competencePanelHeading).append(competenceCode);
-    $(competenceCode).after(competenceName);
-    $(competenceName).after(weightSpan);
-    $(competencePanelHeading).after(competencePanelBody);
-    $(competencePanelBody).after(competencePanelFooter);
-    $(competencePanelFooter).append(subcompetenceButton);
-    $(subcompetenceButton).after(removeCompetenceButton);
 }
-
-function addSubCompetence(competence) {
-    var subcompPanelWrapper = document.createElement('div');
-    var subcompPanel = document.createElement('div');
-    var subcompPanelHeading = document.createElement('div');
-    var subcompPanelBody = document.createElement('div');
-    var subcompPanelFooter = document.createElement('div');
-    var subcompCode = document.createElement('input');
-    var subcompName = document.createElement('input');
-    var indicatorButton = document.createElement('button');
-    var removeSubcompButton = document.createElement('button');
-    var listgroup = document.createElement('ul');
-
-    $(listgroup).addClass('list-group');
-
-    subcompCode.type = 'text';
-    subcompCode.placeholder = "Competence-Code";
-    $(subcompCode).addClass("form-control form-next");
-
-    subcompName.type = 'text';
-    subcompName.placeholder = "Name of the competence";
-    $(subcompName).addClass("form-control form-next");
-
-    $(indicatorButton).addClass("btn");
-    $(indicatorButton).text("Add an indicator");
-    $(indicatorButton).val(competence + "-" + viewModel.subcomp);
-    $(indicatorButton).on('click', function() {
-        addIndicator(listgroup);
-    });
-
-    $(removeSubcompButton).addClass("btn pull-right");
-    $(removeSubcompButton).text("Remove this subcompetence");
-    $(removeSubcompButton).val();
-    $(removeSubcompButton).on('click', function() {
-        $(this).parent().parent().remove();
-    });
-
-    $(subcompPanelWrapper).addClass("subcompPanel panel-" + competence + "-" + viewModel.subcomp);
-    $(subcompPanel).addClass("panel panel-default");
-    $(subcompPanelHeading).addClass("panel-heading color-subcomp");
-    $(subcompPanelBody).addClass("panel-body");
-    $(subcompPanelFooter).addClass("panel-footer color-subcomp");
-
-    $(".panel-body.comp-" + competence).append(subcompPanelWrapper);
-    $(subcompPanelWrapper).append(subcompPanel);
-    $(subcompPanel).append(subcompPanelHeading);
-    $(subcompPanelHeading).append(subcompCode);
-    $(subcompCode).after(subcompName);
-    $(subcompPanelHeading).after(subcompPanelBody);
-    $(subcompPanelBody).append(listgroup);
-    $(subcompPanelBody).after(subcompPanelFooter);
-    $(subcompPanelFooter).append(indicatorButton);
-    $(indicatorButton).after(removeSubcompButton);
-    ++viewModel.subcomp;
-}
-
-function addIndicator(listgroup) {
-    var listItem = document.createElement('li');
-    var indicatorDescription = document.createElement('input');
-    var indicatorName = document.createElement('input');
-    var removeIndicator = document.createElement('button');
-
-    indicatorDescription.type = 'text';
-    indicatorDescription.placeholder = "Description";
-    $(indicatorDescription).addClass("form-control  form-next");
-
-    indicatorName.type = 'text;';
-    indicatorName.placeholder = "Indicatorname";
-    $(indicatorName).addClass('form-control  form-next');
-
-    $(removeIndicator).addClass("btn");
-    $(removeIndicator).text("Remove this indicator");
-    $(removeIndicator).on('click', function() {
-        $(this).parent().remove();
-    });
-
-    $(listItem).addClass("list-group-item");
-
-    $(listgroup).append(listItem);
-    $(listItem).append(indicatorName);
-    $(listItem).append(indicatorDescription);
-    $(listItem).append(removeIndicator);
-}
-
 
 function initPage() {
     viewModel.getProjectInfo();
