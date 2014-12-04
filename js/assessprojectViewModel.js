3ca2d0993b9c21802a54007dc2b688d933804187
Matthieu Calie <matthieu.calie@student.howest.be>
2014-12-04 11:42:16 +0100

    assess project

--- js/assessprojectViewModel.js
+++ js/assessprojectViewModel.js
@@ -1,3 +1,15 @@
-/**
- * Created by Matthieu on 4/12/2014.
- */
+function pageViewModel(gvm) {
+    // projecttitle
+    gvm.projecttitle = ko.observable("");
+
+    // Page specific i18n bindings
+    gvm.title = ko.computed(function (){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("ProjectTitle") + ": " + gvm.projecttitle();}, gvm);
+    gvm.pageHeader = ko.observable("Project");
+    gvm.projectname = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("ProjectName");}, gvm);
+
+    gvm.getProjectInfo = function() {
+        $.getJSON('/api/project/' + $("#projectHeader").data('value'), function(data) {
+            gvm.pageHeader(data[0].code + ' - ' + data[0].name);
+        });
+    };
+}
