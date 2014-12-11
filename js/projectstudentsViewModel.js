228da33f4b95e886201c740aef0432e3e8324f90
Matthieu Calie <matthieu.calie@student.howest.be>
2014-12-11 09:25:21 +0100

    added projectStudents page

--- js/projectstudentsViewModel.js
+++ js/projectstudentsViewModel.js
@@ -4,6 +4,7 @@
 
 // View model for the courses page
 function pageViewModel(gvm) {
+    gvm.projecttitle = ko.observable("");
 
     // Page specific i18n bindings
     gvm.title = ko.computed(function (){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("ProjectTitle") + ": " + gvm.projecttitle();}, gvm);
