5adf269a5a0417a8316f6d9971456fdb57d3dfc5
Matthieu Calie <matthieu.calie@student.howest.be>
2014-12-11 09:24:32 +0100

    added projectStudents page

--- js/projectstudentsViewModel.js
+++ js/projectstudentsViewModel.js
@@ -1,3 +1,25 @@
 /**
  * Created by Matthieu on 11/12/2014.
  */
+
+// View model for the courses page
+function pageViewModel(gvm) {
+
+    // Page specific i18n bindings
+    gvm.title = ko.computed(function (){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("ProjectTitle") + ": " + gvm.projecttitle();}, gvm);
+    gvm.pageHeader = ko.observable("Project");
+    gvm.projectname = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("ProjectName");}, gvm);
+
+    gvm.addCompetenceBtn = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("AddCompetence");}, gvm);
+    gvm.savePage = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("SaveBtn");}, gvm);
+
+    gvm.getProjectInfo = function() {
+        $.getJSON('/api/project/' + $("#projectHeader").data('value'), function(data) {
+            gvm.pageHeader(data[0].code + ' - ' + data[0].name);
+        });
+    };
+}
+
+function initPage() {
+    viewModel.getProjectInfo();
+}
