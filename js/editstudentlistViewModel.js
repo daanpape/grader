d3459dc2f327f8466dc6d224fa7093736b6ce979
Matthieu Calie <matthieu.calie@student.howest.be>
2014-12-11 15:09:40 +0100

    edit student list

--- js/editstudentlistViewModel.js
+++ js/editstudentlistViewModel.js
@@ -24,7 +24,7 @@ function pageViewModel(gvm) {
 }
 
 function initPage() {
-    console.log($("#page-header").value());
+    console.log($("#page-header").data('value'));
     $.getJSON('/api/studentlist/' + $("#page-header").data('value'), function(data) {
         gvm.studentlistName(data[0].name);
     });
