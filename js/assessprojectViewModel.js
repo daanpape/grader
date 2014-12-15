4b8bbf233b7760ea296ff5a4c96318dc4d9659fe
Matthieu Calie <matthieu.calie@student.howest.be>
2014-12-15 10:45:05 +0100

    studentist for assessing project

--- js/assessprojectViewModel.js
+++ js/assessprojectViewModel.js
@@ -19,7 +19,9 @@ function pageViewModel(gvm) {
     };
 
     gvm.getStudentList = function() {
+        console.log("hallo");
         $.getJSON('/api/project/' + $("#projectHeader").data('value') + '/students', function(data) {
+            console.log(data);
             $.each(data.data, function(i, item) {
                 console.log(item);
                 viewModel.addTableData(item.id, item.firstname, item.lastname);
