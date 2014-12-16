620ab273a83be09e90c49facada4d9b5c85f6785
Matthieu Calie <matthieu.calie@student.howest.be>
2014-12-16 16:15:17 +0100

    completeness page

--- index.php
+++ index.php
@@ -322,7 +322,6 @@ $app->post('/api/project/:projectid/documents/:lastid', function($projectid, $la
 
     if(!empty($updateArray))
         $updates = GraderAPI::updateDocuments($updateArray);
-    var_dump($updates);
     if(!empty($insertArray))
         $inserts = GraderAPI::insertDocuments($insertArray);
     echo json_encode("success");
