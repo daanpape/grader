2f8fb4082c16cb228bab8001157cec77bcd9bf75
Matthieu Calie <matthieu.calie@student.howest.be>
2014-12-16 14:45:15 +0100

    completeness page

--- index.php
+++ index.php
@@ -307,7 +307,7 @@ $app->put('/api/project/:projectid/documents/:lastid', function($projectid, $las
     $response = $app->response();
     $response->header('Content-Type', 'application/json');
 
-    $documents = json_decode(stripslashes($_POST['documents']));
+    $documents = json_decode($_POST['documents']);
     var_dump($documents);
     $updateArray = [];
     $insertArray = [];
