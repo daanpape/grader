8e765e33143473560b34683b9506684b76e2cc0d
Matthieu Calie <matthieu.calie@student.howest.be>
2014-12-16 09:14:57 +0100

    completeness page

--- js/projectcompletenessViewModel.js
+++ js/projectcompletenessViewModel.js
@@ -6,7 +6,7 @@ function pageViewModel(gvm) {
     // Page specific i18n bindings
     gvm.title = ko.computed(function (){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("ProjectTitle") + ": " + gvm.projecttitle();}, gvm);
     gvm.pageHeader = ko.observable("Project");
-    gvm.projectCompletenessTitle = ko.computed(function (){i18n.setLocale(gvm.lang); return i18n.__("ProjectCompletenessTitle");}, gvm);
+    //gvm.projectCompletenessTitle = ko.computed(function (){i18n.setLocale(gvm.lang); return i18n.__("ProjectCompletenessTitle");}, gvm);
 
     gvm.getProjectInfo = function() {
         $.getJSON('/api/project/' + gvm.projectId, function(data) {
