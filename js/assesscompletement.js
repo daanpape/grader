92a8b039807759b5414d91b2545c105da9805a9f
Matthieu Calie <matthieu.calie@student.howest.be>
2014-12-16 08:55:04 +0100

    js added

---
+++ js/assesscompletement.js
@@ -0,0 +1,26 @@
+function pageViewModel(gvm) {
+    // projecttitle
+    gvm.projecttitle = ko.observable("");
+    gvm.projectId = $("#projectHeader").data('value');
+
+    // Page specific i18n bindings
+    gvm.title = ko.computed(function (){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("ProjectTitle") + ": " + gvm.projecttitle();}, gvm);
+    gvm.pageHeader = ko.observable("Project");
+    gvm.projectname = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("ProjectName");}, gvm);
+
+    gvm.getProjectInfo = function() {
+        $.getJSON('/api/project/' + gvm.projectId, function(data) {
+            gvm.pageHeader(data[0].code + ' - ' + data[0].name);
+        });
+    };
+
+    /*gvm.getAllData = function() {
+     $.getJSON('/api/project/getAllData/' + $("#projectHeader").data('value'), function(data) {
+     console.log(data);
+     });
+     }*/
+}
+
+function initPage() {
+    viewModel.getProjectInfo();
+}
