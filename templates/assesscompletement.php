4fc93f883073f6dc66308af3502913575ce7e13b
Matthieu Calie <matthieu.calie@student.howest.be>
2014-12-29 21:59:35 +0100

    assess page dropdown updates

--- templates/assesscompletement.php
+++ templates/assesscompletement.php
@@ -40,7 +40,7 @@ $location = "assesscompletement";
                         <td><span data-bind="text: amount_required"></span></td>
                         <td><span data-bind="text: weight"></span></td>
                         <td>
-                            <select class="form-control" data-bind="options: amount_not_submitted, id: 'document-' + id">
+                            <select class="form-control" data-bind="options: amount_not_submitted, attr:{'id': 'document-' + id}">
                             </select>
                         </td>
                     </tr>
