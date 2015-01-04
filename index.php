0490d8bc12f606d69d42298ffe62e94b1ac5ad0e
Matthieu <matthieu.calie@gmail.com>
2015-01-04 15:52:40 +0100

    student list insert

--- index.php
+++ index.php
@@ -333,10 +333,8 @@ $app->put('/api/student/:id', function($id) use ($app) {
     $response->header('Content-Type', 'application/json');
 
     // Update the existing resource
-    /*echo json_encode(GraderAPI::updateStudent(
-        $id, $app->request->post('username'), $app->request->post('firstname'), $app->request->post('lastname')));*/
     echo json_encode(GraderAPI::updateStudent(
-        $id, "test", "test", "test"));
+        $id, $app->request->post('username'), $app->request->post('firstname'), $app->request->post('lastname')));
 });
 
 // API POST routes
