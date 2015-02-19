d46f394814ea743892cb28606938ac42fdc44924
nielsverhaegen <captustv@gmail.com>
2015-02-19 14:54:24 +0100

    lock

--- templates/project.php
+++ templates/project.php
@@ -32,7 +32,9 @@ $location = "project";
                 height:30px;
                 margin-top:10px;
                 background-position-x:-90px;
+                padding-top:10px;
                 display:inline-block;
+
             }
 
             .lock:hover {
