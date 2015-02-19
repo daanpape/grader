8263d62a9c20d4830797a6f5c3b6042d70bd4d9a
nielsverhaegen <captustv@gmail.com>
2015-02-19 14:48:51 +0100

    lock

--- templates/project.php
+++ templates/project.php
@@ -28,8 +28,8 @@ $location = "project";
                 background-image: url("/img/lock.png");
                 background-repeat: no-repeat;
                 background-position-y:-55px;
-                width:25px;
-                height:25px;
+                width:30px;
+                height:30px;
                 margin-top:10px;
                 background-position-x:-90px;
             }
