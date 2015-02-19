5bd18c12d44f205dc80e8876bbb6b213378bc2ff
nielsverhaegen <captustv@gmail.com>
2015-02-19 14:55:39 +0100

    lock

--- templates/project.php
+++ templates/project.php
@@ -32,7 +32,7 @@ $location = "project";
                 height:30px;
                 margin-top:10px;
                 background-position-x:-90px;
-                margin-top:10px;
+                margin-top:30px;
                 display:inline-block;
 
             }
