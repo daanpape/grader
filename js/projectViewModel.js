06367a983ff658eefdfc61a3036701e6f4d86cfc
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-02-19 12:24:41 +0100

    procent check

--- js/projectViewModel.js
+++ js/projectViewModel.js
@@ -86,7 +86,7 @@ function pageViewModel(gvm) {
         // Update automated weight calculation
         var size = gvm.competences.length;
         var percent = 100/size;
-        console.log("Test");
+        console.log(gvm.competences);
         ko.utils.arrayForEach(gvm.competences, function(competence){
             competence.weight(percent);
             alert(percent);
