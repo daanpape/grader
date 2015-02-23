102df243bde5f6555dc9ec5d8cb1f6280363e349
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-02-23 20:25:27 +0100

    lock weight

--- js/projectViewModel.js
+++ js/projectViewModel.js
@@ -14,7 +14,7 @@ function Competence(viewmodel, id, code, name, weight, subcompetences) {
         },
 
         getWeightLeft: function(weightSub){
-            return this.weight - weightSub;
+            return this.weight() - weightSub;
         },
 
         removeThis: function() {
