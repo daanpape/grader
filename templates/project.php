73f811a97d61cf4020c39646a9093cdd6f82a01a
nielsverhaegen <captustv@gmail.com>
2015-02-28 16:02:22 +0100

    lock icons

--- templates/project.php
+++ templates/project.php
@@ -56,7 +56,7 @@ $location = "project";
                         <input type="text" placeholder="Competence-Code" class="form-control form-next" data-bind="value: code">
                         <input type="text" placeholder="Name of the competence" class="form-control form-next" data-bind="value: name">
                         <span>Current weight: <input type="text" class="form-control form-next" data-bind="value: weight">%</span>
-                        <i class="icon-large" data-bind="click: toggleLock, css: {icon-locked: locked == true}"></i>
+                        <i class="icon-large" data-bind="click: toggleLock, css: {icon-lock: locked == true}"></i>
                     </div>
                     <div class="panel-body" data-bind="foreach: subcompetences">         
                         <div class="subcompPanel">
