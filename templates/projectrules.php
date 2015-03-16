62da1ccec497f0cb6d757a81a95d4f0b0221ee83
Jonas Vanalderweireldt <vanal.jonas@hotmail.com>
2015-03-16 13:55:28 +0100

    Test last edit route

--- templates/projectrules.php
+++ templates/projectrules.php
@@ -23,46 +23,7 @@ $location = "projectrules";
     <h1 class="page-header" id="projectHeader" data-value="<?php echo $projectid ?>" data-bind="text: pageHeader">Project</h1>
 </div>
 
-<!-- Content container -->
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <p>Coupled list</p>
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Actions</th>
-                </tr>
-                </thead>
-                <tbody data-bind="foreach: coupledLists">
-                <tr>
-                    <td data-bind="text: tname">--</td>
-                    <td>
-                        <button class="btn" data-bind="attr:{'id': 'uncouplebtn-' + tid}">Uncouple</button>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Actions</th>
-                </tr>
-                </thead>
-                <tbody data-bind="foreach: availableLists">
-                <tr>
-                    <td data-bind="text: tname">--</td>
-                    <td>
-                        <button class="btn" data-bind="attr:{'id': 'couplebtn-' + tid}">Couple</button>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
+TEST
 
 <?php include_once('jsdepends.php') ?>
 </body>
