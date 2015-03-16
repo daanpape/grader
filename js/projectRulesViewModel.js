9160dc08beee04d278bef2df628446a834175ce6
Jonas Vanalderweireldt <vanal.jonas@hotmail.com>
2015-03-16 17:13:31 +0100

    Edit Rulespage

--- js/projectRulesViewModel.js
+++ js/projectRulesViewModel.js
@@ -39,7 +39,7 @@ function pageViewModel(gvm) {
 
     gvm.addProjectAction = function(data) {
         gvm.projectActions.push({ dataAction: data });
-        console.log(data);
+        console.log(gvm.projectActions);
     }
 }
 
