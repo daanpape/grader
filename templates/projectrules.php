e16324321cdf5d5a3f09c00a75916aea7868a821
Jonas Vanalderweireldt <vanal.jonas@hotmail.com>
2015-03-16 17:47:10 +0100

    Edit Rulespage

--- templates/projectrules.php
+++ templates/projectrules.php
@@ -48,7 +48,7 @@ $location = "projectRules";
                                     <span class="pull-right caret-down caret"></span>
                                 </button>
                                 <ul class="dropdown-menu dropdown-location ul-wide" role="menu" aria-labelledby="actions" data-bind="foreach: { data: projectActions, as: 'item' }" id="avaAct">
-                                    <li data-bind="text: item"></li>
+                                    <li data-bind="value: item"></li>
                                     <!--<li class="li-wide" role="presentation"><a role="menuitem" tabindex="-1" href="#" data-bind="attr:{'id': 'actbtn-' + id}"><span data-bind="text: item">test</span></a> </li>-->
                                 </ul>
                             </div>
