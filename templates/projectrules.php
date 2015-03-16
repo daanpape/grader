169c12c3ed710f28b2deb8ed7575aff2870d64c3
Jonas Vanalderweireldt <vanal.jonas@hotmail.com>
2015-03-16 13:45:50 +0100

    Edit projectrules route

--- templates/projectrules.php
+++ templates/projectrules.php
@@ -1,6 +1,6 @@
 <?php
 // Page initialisation
-$location = "project";
+$location = "projectrules";
 ?>
 <!DOCTYPE html>
 <html lang="nl" id="htmldoc">
--- templates/projects.php
+++ templates/projects.php
@@ -83,7 +83,7 @@ $location = "projects";
                             <a data-bind="attr:{'href': '/project/' + tid}"><span class="glyphicon glyphicon-plus glyphicon-btn" data-bind="attr:{'id': 'managebtn-' + tid}"></span></a>
                             <a data-bind="attr:{'href': '/project/students/' + tid}"><span class="glyphicon glyphicon-user glyphicon-btn" data-bind="attr:{'id': 'studentbtn-' + tid}"></span></a>
                             <a data-bind="attr:{'href': '/project/' + tid + '/completeness'}"><span class="glyphicon glyphicon-list glyphicon-btn" data-bind="attr:{'id': 'completenessbtn-' + tid"></span></a>
-                            <a data-bind="attr:{'href': '/project/' + tid + '/completeness'}"><span class="glyphicon glyphicon-check glyphicon-btn" data-bind="attr:{'id': 'completenessbtn-' + tid"></span></a>
+                            <a data-bind="attr:{'href': '/project/' + tid + '/projectrules'}"><span class="glyphicon glyphicon-check glyphicon-btn" data-bind="attr:{'id': 'projectrulesbtn-' + tid"></span></a>
                             <span class="glyphicon glyphicon-trash glyphicon-btn" data-bind="attr:{'id': 'removebtn-' + tid}"></span>
                             </td>
                     </tr>
