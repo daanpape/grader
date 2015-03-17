4ed1721802d643112306c01da9cede5227b87a3f
joachim.cromheecke@live.be <joachim.cromheecke@live.be>
2015-03-17 12:01:39 +0100

    coursesrapporten.php

--- templates/templatesrapport/coursesrapporten.php
+++ templates/templatesrapport/coursesrapporten.php
@@ -18,7 +18,7 @@
     </head>
 
     <body>
-        <?php include_once('menurapporten.php') ?>
+        <?php include_once('templates/menu.php') ?>
 
         <!-- Header container -->
         <div class="container">
