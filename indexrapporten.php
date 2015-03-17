5e50a9882cc88bc7fd9098218354e555d5d958da
Thomas De Wispelaere <thomas.de.wispelaere@student.howest.be>
2015-03-17 11:57:08 +0100

    onze routering

--- indexrapporten.php
+++ indexrapporten.php
@@ -1,7 +1,7 @@
 <?php
 
 $app->get('/coursesrapporten', function () use ($app) {
-    $app->render('templates/templatesrapport/coursesrapporten.php');
+    $app->render('templatesrapport/coursesrapporten.php');
 });
 
 $app->get('/homerapporten', function () use ($app) {
