192f9d5f21809ade6b0e560c8dad32e89d8518ee
nielsverhaegen <captustv@gmail.com>
2015-03-17 11:16:15 +0100

    rules

--- js/projectRulesViewModel.js
+++ js/projectRulesViewModel.js
@@ -83,7 +83,7 @@ function fetchActions() {
     $.getJSON('/api/project/'+ projectid + '/documents', function(data) {
         $.each(data, function(i, item) {
             viewModel.addProjectAction(item.id, item.description);
-            item.description();
+            item.description;
         });
     });
 
