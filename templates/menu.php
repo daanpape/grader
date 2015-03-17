446dd6ad6e011473f3d4bb1189a22048788236f3
joachim.cromheecke@live.be <joachim.cromheecke@live.be>
2015-03-17 11:36:29 +0100

    menu.php

--- templates/menu.php
+++ templates/menu.php
@@ -32,7 +32,7 @@ require_once 'dptcms/config.php';
                 <li class="<?php if ($location == 'courses') {
                     echo 'active';
                 } 
-                ?>"> <a href="templatesrapport/courses.php" data-bind="text: coursestypeBtn">Courses</a></li>
+                ?>"> <a href="/coursesrapporten" data-bind="text: coursestypeBtn">Courses</a></li>
                 
                 <!-- RapportSysteem -->
                     
