7bb0185dcc002b3ea21b900301054c6b43143aff
ZhiyuanChou <zhiyuan.chou@student.howest.be>
2015-03-18 09:47:06 +0100

    kleine update aan de databindingresultaat van hometitle

--- js/jsrapport/homerapportenViewModel.js
+++ js/jsrapport/homerapportenViewModel.js
@@ -1,7 +1,7 @@
 // View model for the page
 function pageViewModel(gvm) {
     // Page specific i18n bindings
-    gvm.title = ko.computed(function (){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("HomeTitle");}, gvm);
+    gvm.title = ko.computed(function (){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("HomeRapportTitle");}, gvm);
     gvm.projectname = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("ProjectName");}, gvm);
     gvm.homeManual = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("HomeRapportManual")}, gvm);
 
