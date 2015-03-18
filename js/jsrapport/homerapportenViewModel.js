733156c116de37814c9604412282cbfb35927b60
ZhiyuanChou <zhiyuan.chou@student.howest.be>
2015-03-18 11:58:52 +0100

    testing console.log bij homerapport

--- js/jsrapport/homerapportenViewModel.js
+++ js/jsrapport/homerapportenViewModel.js
@@ -2,7 +2,9 @@
 function pageViewModel(gvm) {
     // Page specific i18n bindings
     gvm.app  = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("ProjectRapportName")}, gvm);
+    console.log("test 1: " + document.title);
     gvm.title = ko.computed(function (){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("HomeRapportTitle");}, gvm);
+    console.log("test 2: " + document.title);
     gvm.projectname = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("ProjectRapportName");}, gvm);
     gvm.homeManual = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("HomeRapportManual")}, gvm);
 
