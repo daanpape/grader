ad5c01dbc2b7a0f4909d2c2d6af18f14fed037a4
nielsverhaegen <captustv@gmail.com>
2015-03-18 11:56:26 +0100

    save Project rules

--- dptcms/database.php
+++ dptcms/database.php
@@ -786,8 +786,8 @@ class ClassDAO {
     {
         try
         {
-            $conn = Db::getConnection();
             foreach($projectrules as $rule) {
+                $conn = Db::getConnection();
                 if(isset($rule['id']))
                 {
                     $stmt = $conn->prepare("UPDATE rules SET project=?, name=?, action=?, operator=?, value=?, result=? WHERE id=?");
