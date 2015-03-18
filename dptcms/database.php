2474099ce243ec74e9eac6dc59cafde7cf0f8d22
nielsverhaegen <captustv@gmail.com>
2015-03-18 14:26:54 +0100

    save Project rules

--- dptcms/database.php
+++ dptcms/database.php
@@ -796,7 +796,7 @@ class ClassDAO {
                     $stmt->execute(array($id, $rule->name, $rule->action, $rule->operator, (int)$rule->value, (int)$rule->result));
                 }
             }
-            return $conn->lastInsertId();
+            return $data;
         }
         catch (PDOException $ex)
         {
