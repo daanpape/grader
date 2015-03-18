7ee24425e620a8421e1ce0dfbd37e6d64c9317ce
Mathias <mathiasverbanck@gmail.com>
2015-03-18 08:47:48 +0100

    update

--- templates/templatesrapport/assessrapporten.php
+++ templates/templatesrapport/assessrapporten.php
@@ -31,12 +31,33 @@
             <div class="row">
                 <div class="dropdown col-md-4">
                     <button class="btn btn-wide btn-default btn-location dropdown-toggle" type="button" id="locations" data-toggle="dropdown" aria-expanded="true">
-                        <span class="text-center">Choose...</span>
+                        <span class="text-center">Locations</span>
                         <span class="pull-right caret-down caret"></span>
                     </button>
-                    <ul class="dropdown-menu dropdown-location ul-wide" role="menu" aria-labelledby="locations" data-bind="foreach: availableCourses" id="testcliker">
+                    <ul class="dropdown-menu dropdown-location ul-wide" role="menu" aria-labelledby="locations" data-bind="foreach: availableLocations" id="testcliker">
                         <li class="li-wide" role="presentation"><a role="menuitem" tabindex="-1" href="#" data-bind="attr:{'id': 'locbtn-' + id}"><span data-bind="text: name"></span></a> </li>
                     </ul>
+                </div>
+
+                <div class="dropdown col-md-4">
+                    <button class="btn btn-wide btn-default btn-training dropdown-toggle" type="button" id="trainings" data-toggle="dropdown" aria-expanded="true">
+                        <span class="text-center">Trainings</span>
+                        <span class="pull-right caret-down caret"></span>
+                    </button>
+                    <ul class="dropdown-menu dropdown-training ul-wide" role="menu" aria-labelledby="trainings" data-bind="foreach: availableTrainings">
+                        <li class="li-wide" role="presentation"><a role="menuitem" tabindex="-1" href="#" data-bind="attr:{'id': 'trainingbtn-' + id}"><span data-bind="text: name"></span></a></li>
+                    </ul>
+                </div>
+
+                <div class="dropdown col-md-4">
+                    <button class="btn btn-wide btn-default btn-course dropdown-toggle" type="button" id="availableCourses" data-toggle="dropdown" aria-expanded="true">
+                        <span class="text-center">Courses</span>
+                        <span class="pull-right caret-down caret"></span>
+                    </button>
+                    <ul class="dropdown-menu dropdown-course ul-wide" role="menu" aria-labelledby="availableCourses" data-bind="foreach: availableCourses">
+                        <li class="li-wide" role="presentation"><a role="menuitem" tabindex="-1" href="#" data-bind="attr:{'id': 'coursebtn-' + id}"><span data-bind="text: name"></span></a> </li>
+                    </ul>
+
                 </div>
             </div>
         </div>
