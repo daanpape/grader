4b606d506b10a40c00d97f95fec24a17fe440258
joachim.cromheecke@live.be <joachim.cromheecke@live.be>
2015-03-18 14:10:09 +0100

    add new menu

--- js/jsrapport/coursesrapportenViewModel.js
+++ js/jsrapport/coursesrapportenViewModel.js
@@ -231,7 +231,7 @@ function showNewProjectTypeModal()
             </div> \
             <div class="form-group"> \
                 <button class="btn btn-wide btn-default btn-training dropdown-toggle" type="button" id="teacher" data-toggle="dropdown" aria-expanded="true" placeholder="' + i18n.__('TeacherTableTitle') + '" name="teacher"> \
-                    <span class="text-right">Teacher</span> \
+                    <span class="text-left">Teacher</span> \
                     <span class="pull-right caret-down caret"></span> \
                 </button> \
             </div> \
