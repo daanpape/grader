2b72f4b6ad5aae4652af340374496aa1c53df617
Thomas De Wispelaere <thomas.de.wispelaere@student.howest.be>
2015-03-18 10:44:06 +0100

    nieuwe course toevoegen

--- indexrapporten.php
+++ indexrapporten.php
@@ -32,7 +32,7 @@ $app->get('/api/projects/page/:pagenr', function ($pagenr) use ($app) {
     echo json_encode(Pager::genPaginatedAnswer($pagenr, $pagedata));
 });
 
-$app->post('/api/project/', function () use ($app) {
+$app->post('/rapportapi/project/', function () use ($app) {
     ?><script>console.log('im in indexrapporten')</script> <?php
     // Use json headers
     $response = $app->response();
