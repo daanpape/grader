57b1ba8c60850a00d471e3406bf22881080e2434
Mathias <mathiasverbanck@gmail.com>
2015-03-18 13:43:29 +0100

    test

--- js/jsrapport/assessrapportenViewModel.js
+++ js/jsrapport/assessrapportenViewModel.js
@@ -171,9 +171,9 @@ function pageViewModel(gvm) {
 
 function loadTablePage(courseid, pagenr)
 {
-    $.getJSON('/api/projects/' + courseid + '/page/' + pagenr, function(data){
+    /*$.getJSON('/api/projects/' + courseid + '/page/' + pagenr, function(data){
 
-        /* Clear current table page */
+        *//* Clear current table page *//*
         viewModel.clearTable()
 
         // Load table data
@@ -181,7 +181,7 @@ function loadTablePage(courseid, pagenr)
             viewModel.addTableData(item.id, item.code, item.name, item.description);
         });
 
-        /* Let previous en next buttons work */
+        *//* Let previous en next buttons work *//*
         if(data.prev == "none"){
             $('#pager-prev-btn').addClass('disabled');
         } else {
@@ -203,35 +203,35 @@ function loadTablePage(courseid, pagenr)
         // Number of pager buttons
         var numItems = $('.pager-nr-btn').length;
 
-        /* Calculate for the pager buttons */
+        *//* Calculate for the pager buttons *//*
         var lowPage = Math.floor(pagenr/numItems) + 1;
 
         $('.pager-nr-btn').each(function() {
-            /* calculate current page number */
+            *//* calculate current page number *//*
             var thispagenr = lowPage++;
 
-            /* Add the page number */
+            *//* Add the page number *//*
             $(this).html('<a href="#">' + thispagenr + '</a>');
 
-            /* Add active class to current page */
+            *//* Add active class to current page *//*
             if(thispagenr == pagenr) {
                 $(this).addClass('active');
             } else {
                 $(this).removeClass('active');
             }
 
-            /* Disable inactive classes and bind handlers to active classes */
+            *//* Disable inactive classes and bind handlers to active classes *//*
             if(thispagenr > data.pagecount) {
                 $(this).addClass('disabled');
             } else {
-                /* Add click listener for button */
+                *//* Add click listener for button *//*
                 $(this).click(function() {
                     loadTablePage(courseid, thispagenr);
                 });
             }
         });
     });
-
+*/
 }
 
 function initPage() {
