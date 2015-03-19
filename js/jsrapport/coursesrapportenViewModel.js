be1094144d9c6c9236222466e1073ad22cbe905b
joachim.cromheecke@live.be <joachim.cromheecke@live.be>
2015-03-19 16:16:09 +0100

    debug coursesrapportenviewmodel teacher

--- js/jsrapport/coursesrapportenViewModel.js
+++ js/jsrapport/coursesrapportenViewModel.js
@@ -71,7 +71,7 @@ function pageViewModel(gvm) {
         gvm.availableTeacher.removeAll();
         $.each(data, function(i, item) {
             gvm.availableTeacher.push(item);
-
+                console.log("updateteacherfunctie")
             /* Add listener to listitem */
             $("#teacherbtn-" + item.id).click(function(){
                 $(".btn-teacher span:first").text($(this).text());
