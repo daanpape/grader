63b0d8286266d9b9e26ca217af13c5431a251883
Mathias <mathiasverbanck@gmail.com>
2015-03-19 11:07:42 +0100

    test

--- dptcms/database.php
+++ dptcms/database.php
@@ -467,7 +467,7 @@ class ClassDAO {
     public static function getTrainingsByLocation($id) {
         try {
             $conn = Db::getConnection();
-            $stmt = $conn->prepare("SELECT * FROM training WHERE location = :location");
+            $stmt = $conn->prepare("SELECT * FROM course_rapport WHERE location = :location");
             $stmt->bindValue(':location', (int) $id, PDO::PARAM_INT);
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_CLASS);
@@ -485,7 +485,7 @@ class ClassDAO {
     public static function getCoursesByTraining($id) {
         try {
             $conn = Db::getConnection();
-            $stmt = $conn->prepare("SELECT * FROM course_rapport WHERE training = :training");
+            $stmt = $conn->prepare("SELECT * FROM course WHERE training = :training");
             $stmt->bindValue(':training', (int) $id, PDO::PARAM_INT);
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_CLASS);
