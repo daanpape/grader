bef5032bf5fb1429ffd283a4ccbed1b8e438314b
Mathias <mathiasverbanck@gmail.com>
2015-03-20 11:29:10 +0100

    updateModules

--- js/jsrapport/assessrapportenViewModel.js
+++ js/jsrapport/assessrapportenViewModel.js
@@ -138,7 +138,7 @@ function pageViewModel(gvm) {
                     $(".btn-submodule span:first").text($(this).text());
                     gvm.currentSubmoduleId = item.id;
                     gvm.currentGoalId = null;
-                    gvm.updateGoals(gvm.currentGoalId);
+                    gvm.updateGoals(item.id);
                     gvm.saveLastSelectedDropdowns();
                     loadTablePage(item.id, 1);
                     $(".btn-goal span:first").text("Goal");
