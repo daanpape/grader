51ff1fa541ad1c06e77d5f58e64ca2d3cde93faf
jjoachim_jj <joachim.cromheecke@live.be>
2015-03-26 14:13:30 +0100

    coursesrapportenviewmodel teacher

--- indexrapporten.php
+++ indexrapporten.php
@@ -202,4 +202,15 @@ $app->delete('/api/coursedelete/:id', function ($id) use ($app) {
     echo json_encode(RapportAPI::deleteCourse($id));
 });
 
+//API EDIT/UPDATTE STUDENLIST
+$app->put('/api/studentlist/:id', function($id) use ($app){
+    // Use json headers
+    $response = $app->response();
+    $response->header('Content-Type', 'application/json');
+    
+    // Update the existing resource
+    echo json_encode(RapportAPI::updateStudentList(
+    $id, $app->request->post('name')));
+});
+
 ?>
