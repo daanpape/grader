9e7f2d82809b5592ba0807077656c1e95bc24f3e
Jonas Vanalderweireldt <vanal.jonas@hotmail.com>
2015-03-26 11:06:45 +0100

    Admin page

--- admin.php
+++ admin.php
@@ -1,6 +1,10 @@
 <?php
 
 // GET routes
+$app->get('/admin', function () use ($app) {
+    $app->render('admin/index.php');
+});
+
 $app->get('/admin/', function () use ($app) {
     $app->render('admin/index.php');
 });
