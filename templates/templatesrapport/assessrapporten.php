74a912ab8ffa8382fe3bca5615ce3d110a42095f
Mathias <mathiasverbanck@gmail.com>
2015-03-26 10:49:01 +0100

    change tabel assessrapporten

--- templates/templatesrapport/assessrapporten.php
+++ templates/templatesrapport/assessrapporten.php
@@ -76,9 +76,9 @@
             <table class="table table-striped">
                 <thead>
                 <tr>
-                    <th data-bind="text: codeTableTitle">Code</th>
+                    <th data-bind="text: studIDTableTitle">Student ID</th>
                     <th data-bind="text: nameTableTitle">Name</th>
-                    <th data-bind="text: descTableTitle">Description</th>
+                    <th data-bind="text: lastNameTableTitle">Last name</th>
                     <th data-bind="text: mailTableTitle">Mail</th>
                     <th data-bind="text: actionTableTitle">Actions</th>
                 </tr>
