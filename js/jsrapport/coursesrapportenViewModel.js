8407eae724b9d2aa218c694715f7d4d790560fec
jjoachim_jj <joachim.cromheecke@live.be>
2015-03-26 08:52:45 +0100

    coursesrapportenviewmodel teacher

--- js/jsrapport/coursesrapportenViewModel.js
+++ js/jsrapport/coursesrapportenViewModel.js
@@ -83,6 +83,7 @@ function pageViewModel(gvm) {
         console.log("updateteacherfunctie1");
     $.getJSON('/api/teacherrapport/' + id, function(data) {
         gvm.availableTeacher.removeAll();
+        console.log(item);
         $.each(data, function(i, item) {
             gvm.availableTeacher.push(item);
             for (var i = 0; i < gvm.availableTeacher.length; i++) {
