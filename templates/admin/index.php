6eec8feee3111f0940a229759bf159f01e402f22
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-03-26 15:46:07 +0100

    Admin page

--- templates/admin/index.php
+++ templates/admin/index.php
@@ -30,15 +30,12 @@
     <div id="wrapper">
         <!-- Navigation -->
         <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
+            <!--an class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand" href="index.html">Grader Admin</a>
-            </div>
+            </div> -->
+            <?php include_once('../menu.php') ?>
             <!-- /.navbar-header -->
 
             <div class="navbar-default sidebar" role="navigation">
