9a35210a789ec81b48bc2f7ea5a6b14c51af044e
Jonas Vanalderweireldt <vanal.jonas@hotmail.com>
2015-03-26 10:16:07 +0100

    Admin page

--- templates/admin/index.php
+++ templates/admin/index.php
@@ -268,13 +268,13 @@
                             <!-- /input-group -->
                         </li>
                         <li>
-                            <a href="index.php"><i class="fa fa-home fa-fw fa-2x"></i> Dashboard</a>
+                            <a href="index.php"><i class="fa fa-home fa-fw"></i> Dashboard</a>
                         </li>
                         <li>
-                            <a href="permissions.php"><i class="fa fa-wrench fa-fw fa-2x"></i> Rechten</span></a>
+                            <a href="permissions.php"><i class="fa fa-wrench fa-fw"></i> Rechten</span></a>
                         </li>
                         <li>
-                            <a href="users.php"><i class="fa fa-users fa-fw fa-2x"></i> Gebruikers</span></a>
+                            <a href="users.php"><i class="fa fa-users fa-fw"></i> Gebruikers</span></a>
                         </li>
                     </ul>
                 </div>
