ebbc301d4e6ad0452a7fcdb46f536b3d867078c2
Jonas Vanalderweireldt <vanal.jonas@hotmail.com>
2015-03-26 09:31:28 +0100

    Admin page

--- templates/admin/index.php
+++ templates/admin/index.php
@@ -268,7 +268,7 @@
                             <!-- /input-group -->
                         </li>
                         <li>
-                            <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                            <a href="index.php"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
                         </li>
                         <li>
                             <a href="rechten.html"><i class="fa fa-bar-chart-o fa-fw"></i> Rechten</span></a>
