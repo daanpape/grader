0d0697469573ee52280504e31c9157642c80df3d
Jonas Vanalderweireldt <vanal.jonas@hotmail.com>
2015-03-26 11:04:36 +0100

    Admin page

--- admin.php
+++ admin.php
@@ -1,17 +1,12 @@
 <?php
 
 // GET routes
-
-$app->get('/account/admin', function() use ($app) {
-    $app->render('/admin/index.php');
+$app->get('/admin/', function () use ($app) {
+    $app->render('admin/index.php');
 });
 
-$app->get('/account/admin/', function () use ($app) {
-    $app->render('/admin/index.php');
-});
-
-$app->get('/account/admin/home', function () use ($app) {
-    $app->render('/admin/index.php');
+$app->get('/admin/home', function () use ($app) {
+    $app->render('admin/index.php');
 });
 
 ?>
