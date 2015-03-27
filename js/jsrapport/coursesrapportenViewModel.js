421485961affe567c1f5311825eb9db1e0624500
Mathias <mathiasverbanck@gmail.com>
2015-03-27 13:53:31 +0100

    assesrapporten data ophalen studenten (probleem meerdere pagina's proberen oplossen)

--- js/jsrapport/coursesrapportenViewModel.js
+++ js/jsrapport/coursesrapportenViewModel.js
@@ -206,7 +206,7 @@ function loadTablePage(pagenr)
 
         // Load table data
         $.each(data.data, function(i, item) {
-            viewModel.addTableData(item.id, item.code, item.name, item.description);
+            viewModel.addTableData(item.id,  item.firstname, item.lastname, item.mail);
         });
 
         /* Let previous en next buttons work */
