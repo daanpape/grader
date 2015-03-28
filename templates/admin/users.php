3b8e5220721d02981d8a5161f0e2d2b813e05347
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-03-28 17:55:32 +0100

    Admin page: Users tab

--- templates/admin/users.php
+++ templates/admin/users.php
@@ -95,7 +95,7 @@
                         <td>Test FirstName</td>
                         <td>Test LastName</td>
                         <td>Test Status</td>
-                        <td><i class="fa fa-times"></i><i class="fa fa-toggle-off"></i></td>
+                        <td><i class="fa fa-times fa-2x"></i><i class="fa fa-toggle-off fa-2x"></i></td>
                     </tbody>
             </div>
         </div>
