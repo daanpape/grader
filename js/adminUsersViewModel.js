15077128bb6afa514564c5c469b64797a5659b0d
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-03-29 15:49:48 +0200

    Admin page: User tab: Language change

--- js/adminUsersViewModel.js
+++ js/adminUsersViewModel.js
@@ -1,3 +1,11 @@
 function pageViewModel(gvm) {
+    // Page specific i18n bindings
+    gvm.title = ko.computed(function(){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("AdminPage");}, gvm);
+    gvm.pageHeader = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("UserTitle");}, gvm);
 
+    gvm.userName = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("UserName");}, gvm);
+    gvm.firstName = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("FirstName");}, gvm);
+    gvm.lastName = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("LastName");}, gvm);
+    gvm.userStatus = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("UserStatus");}, gvm);
+    gvm.userActions = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("UserActions");}, gvm);
 }
