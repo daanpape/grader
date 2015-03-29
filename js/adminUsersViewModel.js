a4517556b654cba2f3d2dca8b1214f0c2846676c
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-03-29 16:13:28 +0200

    Admin page: get All users

--- js/adminUsersViewModel.js
+++ js/adminUsersViewModel.js
@@ -8,6 +8,8 @@ function pageViewModel(gvm) {
     gvm.lastName = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("Lastname");}, gvm);
     gvm.userStatus = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("UserStatus");}, gvm);
     gvm.userActions = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("UserActions");}, gvm);
+
+    fetchUsersData();
 }
 
 function fetchUsersData()
