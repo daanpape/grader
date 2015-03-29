2ea46219828fbe2634659ffc308481936cb99c18
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-03-29 15:38:15 +0200

    Admin page: Permission tab: Language change

--- templates/admin/jsdepends.php
+++ templates/admin/jsdepends.php
@@ -7,4 +7,3 @@
 <script src="../../js/ie10-viewport-bug-workaround.js"></script>
 <script src="../../js/modals.js"></script>
 <script src="../../js/global.js"></script>
-<script src="../../js/<?php echo $location ?>ViewModel.js"></script>
