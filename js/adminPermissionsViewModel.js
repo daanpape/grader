375792026b7b1f6a64596258b0f0f948b44a6fb0
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-03-29 17:22:11 +0200

    Admin page: get All users roles

--- js/adminPermissionsViewModel.js
+++ js/adminPermissionsViewModel.js
@@ -33,7 +33,7 @@ function fetchUsersData()
             $.each(data, function(i, item)
             {
                 if(item.username == current){
-                    permissions.append(item.role + "\n");
+                    permissions += item.role + "\n");
                 }
             });
 
