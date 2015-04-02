fd93b59016994179dc120cc77df8170822d24525
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-04-02 14:20:53 +0200

    Admin page: Add user

--- js/adminAddUsersViewModel.js
+++ js/adminAddUsersViewModel.js
@@ -4,4 +4,13 @@ function pageViewModel(gvm) {
     gvm.pageHeader = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("UserTitle");}, gvm);
 
     gvm.pageHeaderAddUser = ko.computed(function(){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("UserAddTitle");}, gvm);
+
+
+    gvm.AvailableStatus = ko.observableArray([]);
+    LoadAllStatus();
+}
+
+function LoadAllStatus() {
+    viewModel.AvaailableStatus.push("Active");
+    viewModel.AvailableStatus.push("Non-Active");
 }
