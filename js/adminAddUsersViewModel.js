377b4fa7fb6a694858b4a9752e6571f929551566
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-04-02 14:44:39 +0200

    Admin page: Add user

--- js/adminAddUsersViewModel.js
+++ js/adminAddUsersViewModel.js
@@ -3,6 +3,6 @@ function pageViewModel(gvm) {
     gvm.title = ko.computed(function(){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("AdminPage");}, gvm);
     gvm.pageHeaderAddUser = ko.computed(function(){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("UserAddTitle");}, gvm);
 
-    gvm.saveBtn = ko.computed(function(){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("SaveBtn");}, gvm);
+    gvm.savePage = ko.computed(function(){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("SaveBtn");}, gvm);
 
 }
