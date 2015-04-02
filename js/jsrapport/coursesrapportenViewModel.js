658928929d63d7063e6a57274bf73e1a464913e9
ZhiyuanChou <zhiyuan.chou@student.howest.be>
2015-04-02 13:31:37 +0200

    testing a possible fix for copymethods 2

--- js/jsrapport/coursesrapportenViewModel.js
+++ js/jsrapport/coursesrapportenViewModel.js
@@ -146,7 +146,7 @@ function copyTableItem(id, tblOject) {
         if(val){
             $.ajax({
                 url: "/api/coursecopy/" + id,
-                type: "COPY",
+                type: "post",
                 success: function() {
                     viewModel.tabledata.remove(tblOject);
                 }
