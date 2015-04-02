476570011267bd5425d67f72819e8904e919d0c3
jjoachim_jj <joachim.cromheecke@live.be>
2015-04-02 15:27:07 +0200

    update/edit courses

--- js/jsrapport/coursesrapportenViewModel.js
+++ js/jsrapport/coursesrapportenViewModel.js
@@ -216,7 +216,7 @@ function loadTablePage(pagenr)
 
         // Load table data
         $.each(data.data, function(i, item) {
-            viewModel.addTableData(item.id, item.code, item.name, item.description, item.username);
+            viewModel.addTableData(item.id, item.code, item.name, item.description, item.leerkracht);
         });
         
         /* Let previous en next buttons work */
