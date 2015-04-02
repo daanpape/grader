34158ab4e207d0d980254ef068f88c879bb19e17
ZhiyuanChou <zhiyuan.chou@student.howest.be>
2015-04-02 15:33:16 +0200

    testing dummy 2nd preparedstatement 5

--- dptcms/databaserapporten.php
+++ dptcms/databaserapporten.php
@@ -243,7 +243,8 @@ SELECT code,name,description,leerkracht,active,studentlistid FROM course_rapport
 ");
 
             $stmt2= $conn->prepare(	"  INSERT INTO module_rapport(name,description,course)
-                select name,description from module_rapport where module = :id"
+
+                select name,description from module_rapport where module = :id,  (select id from course_rapport ORDER BY course_rapport.id DESC LIMIT 1)  AS course "
             );
 
             $stmt2->bindValue(':id', (int) $id, PDO::PARAM_INT);
