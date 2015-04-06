4414d4dcdb1ed359f0de631ab77c12d9074ed396
Thomas De Wispelaere <thomas.de.wispelaere@student.howest.be>
2015-04-06 19:06:16 +0200

    changing coursestudents page -> implementing teachers

--- templates/templatesrapport/coursestudentsrapporten.php
+++ templates/templatesrapport/coursestudentsrapporten.php
@@ -23,7 +23,7 @@
     </div>
 
     <div class="container">
-        <h4 class="page-header">Students</h4>
+        <h3 class="page-header">Students</h3>
     </div>
     <!-- Content container -->
     <div class="container">
@@ -50,7 +50,7 @@
     </div>
     
     <div class="container">
-        <h4 class="page-header">Teachers</h4>
+        <h3 class="page-header">Teachers</h3>
     </div>
     <!-- Content container -->
     <div class="container">
