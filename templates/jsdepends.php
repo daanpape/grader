fa27ad587600aa9cb7dbf3c8219a6a6db429671d
Thomas De Wispelaere <thomas.de.wispelaere@student.howest.be>
2015-04-07 16:44:17 +0200

    add students to list

--- templates/jsdepends.php
+++ templates/jsdepends.php
@@ -1,6 +1,7 @@
 <!-- Javascript -->
 <script src="/js/i18n.js"></script>
 <script src="/js/jquery.min.js"></script>
+<script src="/js/jsrapport/jquery-ui.min.js"></script>
 <script src="/js/knockout-3.1.0.js"></script>
 <script src="/js/bootstrap.min.js"></script>
 <script src="/js/ie10-viewport-bug-workaround.js"></script>
