7b86ea821f381e6bf69f5c05ec42539b63512858
nielsverhaegen <captustv@gmail.com>
2015-04-16 16:39:22 +0200

    save to PDF

--- js/assessprojectViewModel.js
+++ js/assessprojectViewModel.js
@@ -17,8 +17,8 @@ function pageViewModel(gvm) {
 
     gvm.getProjectInfo = function() {
         $.getJSON('/api/project/' + gvm.projectId, function(data) {
-            gvm.pageHeader(data[0].code + ' - ' + data[0].name);
-            console.log(data);
+            //gvm.pageHeader(data[0].code + ' - ' + data[0].name);
+            gvm.pageHeader(data);
         });
     };
 
