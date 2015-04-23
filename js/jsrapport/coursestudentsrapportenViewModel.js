256e0b60889b2599fdc241b858848b0c9888a9ca
Thomas De Wispelaere <thomas.de.wispelaere@student.howest.be>
2015-04-23 16:46:51 +0200

    teachers kunnen toevoegen adhv autocomplete

--- js/jsrapport/coursestudentsrapportenViewModel.js
+++ js/jsrapport/coursestudentsrapportenViewModel.js
@@ -121,6 +121,7 @@ function initPage() {
     });
     
     $('#addTeacherBtn').click(function() {
+        console.log(userid);
         addTeacher($('#addTeacherForm').serialize());
         $('#addTeacherForm').hide();
     });
