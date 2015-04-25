6236f4815442730aeb06e077d2b4a8aec401d096
Thomas De Wispelaere <thomas.de.wispelaere@student.howest.be>
2015-04-25 16:20:38 +0200

    teachers + studentlists toevoegen via een Add knop + meerdere teachers per studentlist, vice versa

--- templates/templatesrapport/coursestudentsrapporten.php
+++ templates/templatesrapport/coursestudentsrapporten.php
@@ -42,27 +42,27 @@
         <button type="button" class="btn btn-default pagination-button" id="addCoursemembers">
             <span class="glyphicon glyphicon-plus"></span> <span data-bind="text: addBtn"></span>
         </button>
-
-    </div><br />
-
-    <div id="addStudentListForm" class="container">
-    <div class="row">
-        <label>StudentList:</label>
-        <input id="studentListComplete" />
-        <button id="addStudentListBtn" class="btn btn-default">Add</button>
-    </div>
-    </div>
-
-    <br />
-    
-    <div id="addTeacherForm" class="container">
-        <div class="row">
-            <form method="form">
-                <label>Teacher name:</label>
-                <input id="teachersComplete" name="teachername" />
-                <button id="addTeacherBtn" class="btn btn-default">Add</button>
-            </form>
+        
+        <div id="addStudentListForm" class="container">
+            <div class="row">
+                <label>StudentList:</label>
+                <input id="studentListComplete" />
+                <button id="addStudentListBtn" class="btn btn-default">Add</button>
+            </div>
         </div>
+
+        <br />
+
+        <div id="addTeacherForm" class="container">
+            <div class="row">
+                <form method="form">
+                    <label>Teacher name:</label>
+                    <input id="teachersComplete" name="teachername" />
+                    <button id="addTeacherBtn" class="btn btn-default">Add</button>
+                </form>
+            </div>
+        </div>
+
     </div>
 
     <?php include_once('templates/jsdepends.php') ?>
