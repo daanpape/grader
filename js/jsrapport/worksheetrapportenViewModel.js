f7f88bc7171d8bc4451760f08421d32add0dc7c2
jjoachim_jj <joachim.cromheecke@live.be>
2015-04-27 13:55:29 +0200

    Work sheet pagina rename

--- js/jsrapport/worksheetrapportenViewModel.js
+++ js/jsrapport/worksheetrapportenViewModel.js
@@ -4,6 +4,7 @@ function pageViewModel(gvm) {
     gvm.app  = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("WorksheetNameRapport");}, gvm);
     gvm.title = ko.computed(function (){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("HomeRapportTitle");}, gvm);
     gvm.projectname = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("WorksheetNameRapport");}, gvm);
+    gvm.homeManual = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("WorksheetRapportManual");}, gvm);
 
 }
 
