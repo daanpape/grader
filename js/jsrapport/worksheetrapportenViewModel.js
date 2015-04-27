8760fc28819ff52aaac8bf30ad9710799dc68865
jjoachim_jj <joachim.cromheecke@live.be>
2015-04-27 14:51:53 +0200

    Dropdown worksheet

--- js/jsrapport/worksheetrapportenViewModel.js
+++ js/jsrapport/worksheetrapportenViewModel.js
@@ -6,6 +6,29 @@ function pageViewModel(gvm) {
     gvm.projectname = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("WorksheetNameRapport");}, gvm);
     gvm.homeManual = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("WorksheetRapportManual");}, gvm);
     
+    
+    gvm.updateCourseRapport = function() {
+        $.getJSON('/api/coursedrop', function(data) {
+            gvm.availableCourses.removeAll();
+            $.each(data, function(i, item) {
+                //  Put item in list
+                gvm.availableCourses.push(item);
+
+                // Add listener to listitem
+                $("#coursebtn-" + item.id).click(function(){
+                    gvm.currentCourseId = item.id;
+                    gvm.currentStudentlistId = null;
+                    gvm.currentStudentId = null;
+                    gvm.updateStudentlists(item.id, gvm.userId);
+                    $(".btn-courseRapport span:first").text($(this).text());
+                    $(".btn-studentlist span:first").text("Studentlist");
+                    $('.btn-student span:first').text("Student");
+                    gvm.saveLastSelectedDropdowns();
+                });
+            });
+        });
+    };
+    
 }
 
 function initPage() {
