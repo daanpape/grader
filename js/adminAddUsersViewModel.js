b4a196653207e1d5e62ae031ef0c125ed6216253
nielsverhaegen <captustv@gmail.com>
2015-04-27 17:15:00 +0200

    admin Page

--- js/adminAddUsersViewModel.js
+++ js/adminAddUsersViewModel.js
@@ -1,6 +1,6 @@
 function pageViewModel(gvm) {
     // Page specific i18n bindings
-    gvm.title = ko.computed(function(){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("AdminPage");}, gvm);
+    gvm.title = ko.computed(function(){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("adminAddUser");}, gvm);
     gvm.pageHeaderAddUser = ko.computed(function(){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("UserAddTitle");}, gvm);
 
     gvm.savePage = ko.computed(function(){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("SaveBtn");}, gvm);
