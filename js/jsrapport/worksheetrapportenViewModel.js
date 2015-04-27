d115c594c4501de1bb02070ec98420c2433b48b4
jjoachim_jj <joachim.cromheecke@live.be>
2015-04-27 13:49:02 +0200

    Work sheet pagina

--- js/jsrapport/worksheetrapportenViewModel.js
+++ js/jsrapport/worksheetrapportenViewModel.js
@@ -4,7 +4,6 @@ function pageViewModel(gvm) {
     gvm.app  = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("WorksheetNameRapport");}, gvm);
     gvm.title = ko.computed(function (){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("HomeRapportTitle");}, gvm);
     gvm.projectname = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("WorksheetNameRapport");}, gvm);
-    gvm.homeManual = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("HomeRapportManual");}, gvm);
 
 }
 
