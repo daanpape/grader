87594db2f26773b09e7369a8754fa353bdb7c41e
jjoachim_jj <joachim.cromheecke@live.be>
2015-04-27 14:36:39 +0200

    Dropdown worksheet

--- templates/templatesrapport/worksheetrapporten.php
+++ templates/templatesrapport/worksheetrapporten.php
@@ -20,7 +20,16 @@
         <!-- Content container -->
         <div class="container">
             <div class="big-info"><span data-bind="text: selectCourse">Select course</span>:</div>
-            
+            <div class="row">
+                <div class="dropdown col-md-4">
+                    <button class="btn btn-wide btn-default btn-courseRapport dropdown-toggle" type="button" id="courseRapport" data-toggle="dropdown" aria-expanded="true">
+                        <span class="text-center">Course</span>
+                        <span class="pull-right caret-down caret"></span>
+                    </button>
+                    <ul class="dropdown-menu dropdown-courseRapport ul-wide" role="menu" aria-labelledby="courseRapport" data-bind="foreach: availableCourses" id="testcliker">
+                        <li class="li-wide" role="presentation"><a role="menuitem" tabindex="-1" href="#" data-bind="attr:{'id': 'coursebtn-' + id}"><span data-bind="text: name"></span></a> </li>
+                    </ul>
+                </div>            
         </div>
 
         <?php include_once('templates/jsdepends.php') ?>
