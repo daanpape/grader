8d9575a8ec6f08323f43cdaabf0d6ca174349b64
nielsverhaegen <captustv@gmail.com>
2015-04-27 17:19:01 +0200

    admin Page

--- templates/admin/adduser.php
+++ templates/admin/adduser.php
@@ -136,7 +136,7 @@ $location = "adminAddUser";
             <div class="container">
                 <div class="row">
                     <div id="bottom-col" class="col-md-12">
-                        <button class="btn btn-lg savePageBtn pull-right" data-bind="click: createUser">Save</button>
+                        <button class="btn btn-lg savePageBtn pull-right" data-bind="click: createUser()">Save</button>
                     </div>
                 </div>
             </div>
