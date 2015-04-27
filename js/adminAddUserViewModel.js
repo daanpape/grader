b0a46e2f2c89024d46056f045533581c3c567754
nielsverhaegen <captustv@gmail.com>
2015-04-27 17:47:58 +0200

    admin Page

--- js/adminAddUserViewModel.js
+++ js/adminAddUserViewModel.js
@@ -9,7 +9,7 @@ function pageViewModel(gvm) {
 function createNewUser()
 {
     console.log("Fired");
-    /*$.ajax({
+    $.ajax({
      type: "POST",
      url: "register",
      data: $('#addUserForm').serialize(),
@@ -18,7 +18,7 @@ function createNewUser()
      },
      error: function() {
      }
-     });*/
+     });
 }
 
 function initPage() {
