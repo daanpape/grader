8902d9a47851c6d9c18e2d1208c325bb6210b268
jjoachim_jj <joachim.cromheecke@live.be>
2015-04-27 13:06:34 +0200

    Work sheet pagina NL

--- js/jsrapport/worksheetrapporten.js
+++ js/jsrapport/worksheetrapporten.js
@@ -1,9 +1,9 @@
 // View model for the page
 function pageViewModel(gvm) {
     // Page specific i18n bindings
-    gvm.app  = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("ProjectRapportName")}, gvm);
+    gvm.app  = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("WorksheetNameRapport")}, gvm);
     gvm.title = ko.computed(function (){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("HomeRapportTitle");}, gvm);
-    gvm.projectname = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("ProjectRapportName");}, gvm);
+    gvm.projectname = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("WorksheetNameRapport");}, gvm);
 
 }
 
