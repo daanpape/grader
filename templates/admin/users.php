6e1682f7e0da1de1c1e695616f022e5dff44abc3
Jonas Vanalderweireldt <vanal.jonas@hotmail.com>
2015-04-28 11:03:49 +0200

    admin user

--- templates/admin/users.php
+++ templates/admin/users.php
@@ -83,7 +83,7 @@ $location = "adminUsers";
                 <h1 class="page-header" data-bind="text: pageHeader">Gebruikers</h1>
             </div>
             <div class="col-lg-1">>
-                <a href="#" type="button" class="btn btn-default pagination-button" id="addUser">
+                <a href="" type="button" class="btn btn-default pagination-button" id="addUser">
                     <span class="glyphicon glyphicon-plus"></span> <span data-bind="text: addBtn, click: addNewUser"></span>
                 </a>
             </div>
