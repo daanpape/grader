0c9fff5420290cb59db7bca8f43dbd4b1c60d636
Thomas De Wispelaere <thomas.de.wispelaere@student.howest.be>
2015-04-28 10:10:46 +0200

    werkfiches kunnen toevoegen via worksheet page

--- js/jsrapport/worksheetrapportenViewModel.js
+++ js/jsrapport/worksheetrapportenViewModel.js
@@ -14,6 +14,8 @@ function pageViewModel(gvm) {
     gvm.werkficheName = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("werkficheName");}, gvm);
     gvm.werkficheAction = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("werkficheAction");}, gvm);
     
+    gvm.currentCourseId = null;
+    
     gvm.updateCourseRapport = function() {
         $.getJSON('/api/coursedrop', function(data) {
             gvm.availableCourses.removeAll();
@@ -24,11 +26,7 @@ function pageViewModel(gvm) {
                 // Add listener to listitem
                 $("#coursebtn-" + item.id).click(function(){
                     gvm.currentCourseId = item.id;
-                    gvm.currentStudentlistId = null;
-                    gvm.currentStudentId = null;
                     $(".btn-courseRapport span:first").text($(this).text());
-                    $(".btn-studentlist span:first").text("Studentlist");
-                    $('.btn-student span:first').text("Student");
                 });
             });
         });
