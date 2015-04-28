9a96d70a01a2819cbf7544c041367a414faca77a
Mathias <mathiasverbanck@gmail.com>
2015-04-28 11:42:12 +0200

    table load functies coursesstudentsrapporten

--- js/jsrapport/coursestudentsrapportenViewModel.js
+++ js/jsrapport/coursestudentsrapportenViewModel.js
@@ -191,15 +191,15 @@ function getGroupid() {
 function loadTablePage(pagenr)
 {
     console.log("loadTablePage");
-    $.getJSON('/api/coursesrapport/page/' + pagenr, function(data){
+    $.getJSON('/api/getStudentGroupTeacherByCourseID/' + pagenr, function(data){
 
         /* Clear current table page */
         viewModel.clearTable();
 
         // Load table data
         $.each(data.data, function(i, item) {
-            viewModel.addTableData(item.id, item.code, item.name);
-            console.log(item.id);
+            viewModel.addTableData(item.id, item.firstname, item.lastname);
+            console.log(item.firstname);
         });
 
         /* Let previous en next buttons work */
