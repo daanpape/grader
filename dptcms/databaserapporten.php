7101c6110c410659e2cd216600fae10bc7c1b406
Mathias <mathiasverbanck@gmail.com>
2015-04-28 09:16:12 +0200

    New method getStudentGroupTeacherByCourseID

--- dptcms/databaserapporten.php
+++ dptcms/databaserapporten.php
@@ -201,7 +201,7 @@ class rapportenDAO {
             $stmt = $conn->prepare("SELECT *FROM  `course_studentlist_teacher_rapport`
                                       LEFT JOIN `studentlist_rapport` ON course_studentlist_teacher_rapport.studentlist = studentlist_rapport.id
                                       LEFT JOIN 'users' ON course_studentlist_teacher_rapport.teacher = user.id
-                                    WHERE  `course` = 1");
+                                    WHERE  course = :course");
             $stmt->bindValue(':course', (int) $courseid, PDO::PARAM_INT);
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_CLASS);
