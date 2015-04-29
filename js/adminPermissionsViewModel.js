8b2c6487758899ef8b6cd17feb007d586e7633ea
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-04-29 16:19:30 +0200

    Admin edit user

--- js/adminPermissionsViewModel.js
+++ js/adminPermissionsViewModel.js
@@ -39,6 +39,10 @@ function fetchUsersData()
 
             permissions = permissions.substr(0, permissions.length - 3);
 
+            if (permissions == null){
+                permissions = "Nog geen rechten toegekend";
+            }
+
             if (addedUsername != current){
                 addedUsername = item.username;
                 viewModel.updateUsersPermissions(new User(item.userid, item.username, item.firstname, item.lastname, permissions));
