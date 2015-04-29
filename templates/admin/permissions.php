7875717f70833e499cb59a4c95fbda788ec4a7cf
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-04-29 15:20:16 +0200

    Admin edit user

--- templates/admin/permissions.php
+++ templates/admin/permissions.php
@@ -148,7 +148,7 @@ $location = "adminPermissions";
 </div>
 <!-- /#wrapper -->
 
-<?php include_once('../jsdepends.php') ?>
+<?php include_once('adminjsdepends.php') ?>
 
 <!-- jQuery -->
 <script src="/admin_static/bower_components/jquery/dist/jquery.min.js"></script>
