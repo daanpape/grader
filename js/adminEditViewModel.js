6561e6e99ef3b2726ed447ae9c3216afe81aba54
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-04-29 19:45:40 +0200

    Admin edit user

--- js/adminEditViewModel.js
+++ js/adminEditViewModel.js
@@ -113,7 +113,7 @@ function checkPermissions(){
     });
 
     $.each(viewModel.checkedRights(), function(i, item){
-        console.log(" - " + item);
+        console.log(" - " + item["item"]);
     });
 }
 
