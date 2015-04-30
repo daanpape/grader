b707ec889a90335e1b7e24d6bdc4b5172b93d7ac
nielsverhaegen <captustv@gmail.com>
2015-04-30 15:04:20 +0200

    grading engine

--- grading_engine/gradingengine.php
+++ grading_engine/gradingengine.php
@@ -128,12 +128,12 @@ class GradingEngine {
         //return $projectStructure[0]->subcompetences[0]->indicators[0]->score;
 
         $indicatorCount = 0;
-        /*foreach($score as $point)
+        foreach($score as $point)
         {
-            $projectStructure[$point->competence]->subcompetences[$point->subcompetence]->indicators[$point->indicator]->score = 1;
-        }*/
+            $projectStructure[$point['competence']]->subcompetences[$point['subcompetence']]->indicators[$point['indicator']]->score = 1;
+        }
 
-        return $score[0]['competence'];
+        return $projectStructure;
 
 
 
