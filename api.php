1ab0bb48d8ed4c265cf415f95da93afcc9ce01bf
nielsverhaegen <captustv@gmail.com>
2015-04-30 12:00:35 +0200

    grading engine

--- api.php
+++ api.php
@@ -80,7 +80,7 @@ class GraderAPI {
 
     public static function gradeProjectForStudent($projectid, $userid)
     {
-        return GradingEngine::gradeProjectForStudent($projectid,$userid);
+        return ClassDAO::getAllScoresForStudentByProject($projectid,$userid);
     }
 
     /*
