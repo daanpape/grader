0dcf5c193913b24bcbba3112e0bf42d987a8328d
nielsverhaegen <captustv@gmail.com>
2015-04-30 13:46:06 +0200

    grading engine

--- grading_engine/gradingengine.php
+++ grading_engine/gradingengine.php
@@ -88,14 +88,14 @@ class GradingEngine {
         $projectRules = array();
         foreach($rules as $rule)
         {
-            $newRule = new Rule();
+            /*$newRule = new Rule();
             $newRule->type = $rule->action['subject'];
             $newRule->id = $rule->action['id'];
             $newRule->operator = $rule->operator;
             $newRule->value = $rule->result;
             $newRule->sign = $rule->sign;
             $newRule->percent = $rule->value;
-            array_push($projectRules, $newRule);
+            array_push($projectRules, $newRule);*/
         }
         return $projectRules;
     }
