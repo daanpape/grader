0fafbc7c144a53087866c84670537f38cb87823c
nielsverhaegen <captustv@gmail.com>
2015-04-30 14:32:27 +0200

    grading engine

--- grading_engine/gradingengine.php
+++ grading_engine/gradingengine.php
@@ -119,7 +119,7 @@ class GradingEngine {
         }
 
         // Calculate indicator points
-        return $projectStructure[0]->subcompetences[0]->indicators[0];
+        return $projectStructure[0]->subcompetences[0]->indicators[0]->score;
 
 
 
