ce4304c93469fd9ebbc7eeb97001dfdc26d74256
Mathias <mathiasverbanck@gmail.com>
2015-04-30 10:35:52 +0200

    Make it possible to add worksheets for a specific course.

--- js/jsrapport/assessrapportenViewModel.js
+++ js/jsrapport/assessrapportenViewModel.js
@@ -183,7 +183,7 @@ function getWorksheetid() {
 }
 
 function addWorksheet($worksheetid) {
-    console.log("toe te voegenworksheet " + $worksheetsid)
+    console.log("toe te voegenworksheet " + $worksheetid)
     /*
     $.ajax({
         url: "/api/coursecouple/" + courseid + "/" + studlijstid + "/" + teacherid,
