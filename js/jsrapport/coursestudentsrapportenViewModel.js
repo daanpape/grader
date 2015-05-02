3af7f9b4704b61848f4c78384584df271b830270
Thomas De Wispelaere <thomas.de.wispelaere@student.howest.be>
2015-05-02 11:32:21 +0200

    viewmodels/functies opkuisen

--- js/jsrapport/coursestudentsrapportenViewModel.js
+++ js/jsrapport/coursestudentsrapportenViewModel.js
@@ -17,77 +17,6 @@ function pageViewModel(gvm) {
     
     gvm.addBtn = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("AddBtn")}, gvm);
 
-    gvm.addmoduleBtn = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("Addmodule");}, gvm);
-    gvm.savePage = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("SaveBtn");}, gvm);
-
-    /*gvm.getProjectInfo = function() {
-        $.getJSON('/api/project/' + $("#projectHeader").data('value'), function(data) {
-            gvm.pageHeader(data[0].code + ' - ' + data[0].name);
-        });
-    };*/
-
-    //gvm.coupledLists = ko.observableArray([]);
-    //gvm.availableLists = ko.observableArray([]);
-
-    /*gvm.addCoupledList = function(id, name) {
-        // Push data
-        var tblOject = {tid: id, tname: name};
-        gvm.coupledLists.push(tblOject);
-
-        $("#uncouplebtn-" + id).click(function(){
-            showYesNoModal("Bent u zeker dat u dit item wil ontkoppelen?", function(val){
-                if(val){
-                    $.ajax({
-                        url: '/api/project/' + $("#projectHeader").data('value') +'/studentlist/uncouple/' + id,
-                        type: "DELETE",
-                        success: function() {
-                            gvm.coupledCount--;
-                            viewModel.coupledLists.remove(tblOject);
-                        }
-                    });
-
-                }
-            });
-        });
-    }*/
-
-    /*gvm.addAvailableLists = function(id, name) {
-        var tblOject = {tid: id, tname: name};
-        gvm.availableLists.push(tblOject);
-
-        $("#couplebtn-" + id).click(function(){
-            if(gvm.coupledCount == 0) {
-                $.ajax({
-                    url: '/api/project/' + $("#projectHeader").data('value') + '/studentlist/' + id,
-                    type: "POST",
-                    success: function() {
-                        gvm.coupledCount++;
-                        viewModel.coupledLists.push(tblOject);
-                    }
-                });
-            } else {
-                alert("list already coupled");
-            }
-        });
-    }*/
-
-    /*gvm.getCoupledLists = function() {
-        $.getJSON('/api/project/' + $("#projectHeader").data('value') + '/coupledlists', function(data) {
-            $.each(data, function(i, item) {
-                gvm.coupledCount++;
-                gvm.addCoupledList(item.id, item.name);
-            });
-        });
-    }*/
-
-    /*gvm.getAvailableLists = function() {
-        $.getJSON('/api/studentlists/' + gvm.userId, function(data) {
-            $.each(data, function(i, item) {
-                gvm.addAvailableLists(item.id, item.name);
-            });
-        });
-    }*/
-
     // The table data observable array
     gvm.tabledata = ko.observableArray([]);
 
@@ -280,9 +209,6 @@ function deleteTableItem(id, tblOject) {
 }
 
 function initPage() {
-    //viewModel.getProjectInfo();
-    //viewModel.getCoupledLists();
-
     $('#addGroupForm').hide();
     
     $('#addCoursemembers').click(function() {
@@ -305,7 +231,6 @@ function initPage() {
     $.getJSON('/api/currentuser', function(data) {
         viewModel.userId = data.id;
         userid = data.id;
-        //viewModel.getAvailableLists(data.id);
     });
     loadTablePage(1,$('#projectHeader').attr("data-value"));
 }
