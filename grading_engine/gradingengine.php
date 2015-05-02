94f074ce450feca07cc9f8c07a0fb963d66b6ae5
nielsverhaegen <captustv@gmail.com>
2015-05-02 14:06:15 +0200

    grading engine

--- grading_engine/gradingengine.php
+++ grading_engine/gradingengine.php
@@ -177,7 +177,7 @@ class GradingEngine {
 
         // Calculate the rules
 
-        foreach($rules as $rule)
+        /*foreach($rules as $rule)
         {
             if($rule->action['subject'] == "subcompetence")
             {
@@ -197,7 +197,7 @@ class GradingEngine {
                     }
                 }
             }
-        }
+        }*/
 
         // Final Point for project
 
@@ -205,7 +205,7 @@ class GradingEngine {
 
 
 
-        return $projectStructure;
+        return $projectStructure->subcompetences;
 
     }
 }
