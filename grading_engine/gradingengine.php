eaa5c208f52cf9a34440656102bf588d4dbbf96f
nielsverhaegen <captustv@gmail.com>
2015-05-02 14:04:28 +0200

    grading engine

--- grading_engine/gradingengine.php
+++ grading_engine/gradingengine.php
@@ -183,18 +183,18 @@ class GradingEngine {
             {
                 if($rule->operator == "<")
                 {
-                    /*foreach($projectStructure->$competence->subcompetences as $subcompetence)
+                    foreach($projectStructure->$competence->subcompetences as $subcompetence)
                     {
-                        if($rule->action['id'] === $subcompetence->id)
+                        /*if($rule->action['id'] == $subcompetence->id)
                         {
                             if($subcompetence->score < $rule->result) {
-                                if ($rule->sign === "-")
+                                if ($rule->sign == "-")
                                 {
                                     $subcompetence->score -= (($rule->value / 100) * $subcompetence->score);
                                 }
                             }
-                        }
-                    }*/
+                        }*/
+                    }
                 }
             }
         }
