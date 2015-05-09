ef250cf89d26eb8a919c4beeb4edd54b5a08ded3
nielsverhaegen <captustv@gmail.com>
2015-05-09 15:50:28 +0200

    grader engine

--- js/assessViewModel.js
+++ js/assessViewModel.js
@@ -233,29 +233,4 @@ function initPage() {
         viewModel.userId = data.id;
         viewModel.updateDropdowns();
     });
-
-    init();
-}
-
-function init()
-{
-    // Tooltip only Text
-    $('.masterTooltip').hover(function(){
-        // Hover over code
-        var title = $(this).attr('title');
-        $(this).data('tipText', title).removeAttr('title');
-        $('<p class="tooltip"></p>')
-            .text(title)
-            .appendTo('body')
-            .fadeIn('slow');
-    }, function() {
-        // Hover out code
-        $(this).attr('title', $(this).data('tipText'));
-        $('.tooltip').remove();
-    }).mousemove(function(e) {
-        var mousex = e.pageX + 20; //Get X coordinates
-        var mousey = e.pageY + 10; //Get Y coordinates
-        $('.tooltip')
-            .css({ top: mousey, left: mousex })
-    });
 }
