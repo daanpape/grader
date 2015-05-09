b34d4bca987e7927bdbb2722c8415bc33d1d5e62
Thomas De Wispelaere <thomas.de.wispelaere@student.howest.be>
2015-05-09 12:03:32 +0200

    worksheetassess page - datepicker

--- js/jsrapport/worksheetassessViewModel.js
+++ js/jsrapport/worksheetassessViewModel.js
@@ -7,15 +7,53 @@ function pageViewModel(gvm) {
     gvm.homeManual = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("WorksheetRapportManual");}, gvm);
     
     gvm.userId = null;
+    gvm.courseId = null;
     
     gvm.formdate = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("FormDate");}, gvm);
     gvm.formmodules = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("FormModules");}, gvm);
     gvm.formworksheet = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("FormWorksheet");}, gvm);
+    
+    gvm.modules = ko.observableArray([]);
+    
+    gvm.updateModules = function(courseid) {
+        $.getJSON('/api/coursestructure/' + courseid, function(data) {
+            if (!$.isEmptyObject(data)) {
+                gvm.modules.removeAll();
+                
+                $.each(data, function(i, item) {
+                    var tblObject = {modid: item.id, modname: item.name, competences: new Array()};
+                    if (item.doelstellingen[Object.keys(item.doelstellingen)[0]].id !== null) {
+                        gvm.updateCompetences(item.doelstellingen, tblObject.competences);
+                    }
+                    gvm.modules.push(tblObject);
+                });
+            }
+        });
+    }
+    
+    gvm.updateCompetences = function(data, competences) {          
+        $.each(data, function(i, item) {
+            var tblObject = {comid: item.id, comname: item.name, criterias: new Array()}
+            if (item.criterias[Object.keys(item.criterias)[0]] !== null) {
+                gvm.updateCriteria(item.criterias, tblObject.criterias);
+            }
+            competences.push(tblObject);
+        });
+    }
+    
+    gvm.updateCriteria = function(data, criteria) {     
+        $.each(data, function(i, item){
+            var tblObject = {critid: item.id, critname: item.name};
+            criteria.push(tblObject);
+        });
+    }
 }
     
 function initPage() {      
     $.getJSON('/api/currentuser', function(data) {
         viewModel.userId = data.id;
+        viewModel.courseId = $('#storage').attr('data-value');
+        viewModel.updateModules(viewModel.courseId);
     });
     
     $('#date').datepicker();
--- templates/templatesrapport/worksheetassess.php
+++ templates/templatesrapport/worksheetassess.php
@@ -13,6 +13,7 @@
         <?php include_once('templates/menu.php') ?>
 
         <!-- Header container -->
+        <p id="storage" data-bind="attr:{'data-value': <?php echo $courseid ?>}" style="display: none"></p>
         <div class="container">
             <h1 id="header" class="page-header" data-bind="attr:{'data-value': <?php echo $workid ?>}"><?php echo $workname ?></h1>
         </div>
@@ -22,14 +23,41 @@
                 <div class="col-md-12">
                     <form id="worksheetassess" action="/assessrapporten">
                         <div class="form-group">
-                            <label data-bind="text: formdate"></label>
+                            <label data-bind="text: formdate"></label><br />
                             <input type="text" id="date" />
                         </div>
                         <br />
                         
                         <div class="form-group">
                             <label data-bind="text: formmodules"></label>
-                            <!-- modules, competences and criteria scores -->
+                            <div class="container" data-bind="foreach: modules">
+                                <div class="col-md-12 compPanel">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading">
+                                            <input type="text" placeholder="module name" class="form-control form-name" data-bind="value: tname">
+                                            <!-- assess input field -->
+                                        </div>
+                                        <div class="panel-body" data-bind="foreach: competences">         
+                                            <div class="subcompPanel">
+                                                <div class="panel panel-default">
+                                                    <div class="panel-heading color-subcomp">
+                                                        <input type="text" placeholder="competence name" class="form-control form-name" data-bind="value: tname">
+                                                        <!-- assess input field -->
+                                                    </div>
+                                                    <div class="panel-body">
+                                                        <ul class="list-group" data-bind="foreach: criterias">
+                                                            <li class="list-group-item">
+                                                                <input type="text" placeholder="criteria name" class="form-control form-name" data-bind="value: tname">
+                                                                <!-- assess input field -->
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <br />
                         
