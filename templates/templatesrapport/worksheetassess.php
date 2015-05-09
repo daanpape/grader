ace1738283d545970fbc4a549f25bc8b1735ad6c
Thomas De Wispelaere <thomas.de.wispelaere@student.howest.be>
2015-05-09 12:52:53 +0200

    worksheetassess page - modules

--- templates/templatesrapport/worksheetassess.php
+++ templates/templatesrapport/worksheetassess.php
@@ -32,9 +32,11 @@
                             <div class="container" data-bind="foreach: modules">
                                 <div class="col-md-12 compPanel">
                                     <div class="panel panel-default">
-                                        <div class="panel-heading">                                              
-                                            <input type="text" placeholder="module name" class="form-control form-name col-xs-3" data-bind="value: modname">
-                                            <!--<div class="dropdown col-md-2">
+                                        <div class="panel-heading">    
+                                            <div class="col-xs-5">                                                
+                                                <input type="text" placeholder="module name" class="form-control form-name col-xs-3" data-bind="value: modname">
+                                            </div>
+                                            <div class="dropdown col-md-2">
                                                 <button class="btn btn-wide btn-default btn-assessScore dropdown-toggle" type="button" id="assessScore" data-toggle="dropdown" aria-expanded="true">
                                                     <span class="text-center">Choose...</span>
                                                     <span class="pull-right caret-down caret"></span>
@@ -42,13 +44,15 @@
                                                 <ul class="dropdown-menu dropdown-assessMethod ul-wide" role="menu" aria-labelledby="assessScore" name="assessScore" data-bind="assessMethod">
                                                     <li class="li-wide" role="presentation"><a role="menuitem" tabindex="-1" href="#" data-bind="score"></a></li>
                                                 </ul>
-                                            </div>-->
+                                            </div>
                                         </div>
                                         <div class="panel-body" data-bind="foreach: competences">         
                                             <div class="subcompPanel">
                                                 <div class="panel panel-default">
                                                     <div class="panel-heading color-subcomp">
-                                                        <input type="text" placeholder="competence name" class="form-control form-name" data-bind="value: comname">
+                                                        <div class="col-xs-5">
+                                                            <input type="text" placeholder="competence name" class="form-control form-name" data-bind="value: comname">
+                                                        </div>
                                                         <div class="dropdown col-md-2">
                                                             <button class="btn btn-wide btn-default btn-assessScore dropdown-toggle" type="button" id="assessScore" data-toggle="dropdown" aria-expanded="true">
                                                                 <span class="text-center">Choose...</span>
@@ -62,7 +66,9 @@
                                                     <div class="panel-body">
                                                         <ul class="list-group" data-bind="foreach: criterias">
                                                             <li class="list-group-item">
-                                                                <input type="text" placeholder="criteria name" class="form-control form-name" data-bind="value: critname">
+                                                                <div class="col-xs-5">
+                                                                    <input type="text" placeholder="criteria name" class="form-control form-name" data-bind="value: critname">
+                                                                </div>
                                                                 <div class="dropdown col-md-2">
                                                                     <button class="btn btn-wide btn-default btn-assessScore dropdown-toggle" type="button" id="assessScore" data-toggle="dropdown" aria-expanded="true">
                                                                         <span class="text-center">Choose...</span>
