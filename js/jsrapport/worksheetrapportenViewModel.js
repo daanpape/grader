2e587b905df483e29284a54da2ffe2f27214db8e
ZhiyuanChou <zhiyuan.chou@student.howest.be>
2015-05-09 11:54:57 +0200

    testing a logging method in coursestudents and worksheetrapporten

--- js/jsrapport/worksheetrapportenViewModel.js
+++ js/jsrapport/worksheetrapportenViewModel.js
@@ -170,6 +170,8 @@ function loadTablePage(pagenr)
         // Load table data
         $.each(data.data, function(i, item) {
             viewModel.addTableData(item.id, item.Name);
+            console.log(item.id +" "+ item.Name);
+            console.log(data)
         });
         
         /* Let previous en next buttons work */
