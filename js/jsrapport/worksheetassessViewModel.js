4071aedfb67147a8ccac56b52149582634efec61
Thomas De Wispelaere <thomas.de.wispelaere@student.howest.be>
2015-05-11 13:48:36 +0200

    worksheetassess page - assess modules with observables

--- js/jsrapport/worksheetassessViewModel.js
+++ js/jsrapport/worksheetassessViewModel.js
@@ -71,11 +71,11 @@ function pageViewModel(gvm) {
 function fillArray(array) {
     $.each(array, function(i, item) {
         viewModel.assessMethod.push({score: item});
-    });
-    $('ul.dropdown-assessMethod li a').click(function(e){
-        $(this).parent().parent().parent().find('.btn-assessScore span:first').text($(this).text());
-        console.log($(this).parent().parent().parent().find('.btn-assessScore span:first').val() + ": " + $(this).parent().parent().parent().find('.btn-assessScore span:first').text());
-        e.preventDefault();
+        $('ul.dropdown-assessMethod li a').click(function(e){
+            $(this).parent().parent().parent().find('.btn-assessScore span:first').text($(this).text());
+            console.log($(this).parent().parent().parent().find('.btn-assessScore span:first').val() + ": " + $(this).parent().parent().parent().find('.btn-assessScore span:first').text());
+            e.preventDefault();
+        });
     });
 }
 
