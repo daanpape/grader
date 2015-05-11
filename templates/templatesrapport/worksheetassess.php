aafd5c80fd4b449e981ea884dd326d4c42743ca0
Thomas De Wispelaere <thomas.de.wispelaere@student.howest.be>
2015-05-11 12:39:47 +0200

    worksheetassess page - assess modules with observables

--- templates/templatesrapport/worksheetassess.php
+++ templates/templatesrapport/worksheetassess.php
@@ -76,7 +76,7 @@
                                                         <input type="text" placeholder="competence name" class="form-control form-name" data-bind="value: comname">
                                                         <div class="dropdown col-md-2">
                                                             <button class="btn btn-wide btn-default btn-assessScore dropdown-toggle" type="button" id="assessScore" data-toggle="dropdown" aria-expanded="true">
-                                                                <span class="text-center" data-bind="valueWithInit: score">Choose...</span>
+                                                                <span class="text-center" data-bind="value: score">Choose...</span>
                                                                 <span class="pull-right caret-down caret"></span>
                                                             </button>
                                                             <ul class="dropdown-menu dropdown-assessMethod ul-wide" role="menu" aria-labelledby="assessScore" name="assessScore" data-bind="foreach: viewModel.assessMethod">
