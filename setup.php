bf8bdf5279ed50814c9128cf6d5825abc1f2086b
Glenn Matthys <glenn@webmind.be>
2015-05-14 15:19:53 +0200

    Include bootstrap CSS in setup

--- setup.php
+++ setup.php
@@ -499,13 +499,15 @@ if(@$filteredGET['mode'] == 'json')
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <script type="text/javascript" src="js/jquery.min.js"></script>
         <script type="text/javascript" src="js/knockout-3.1.0.js"></script>
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
         <style type="text/css">
-            body { font-family: DejaVu Sans; }
             span.rpasslink { font-size: small; }
             span.rpasslink:hover { cursor: pointer; }
             div[id^=Step] { display: none; }
