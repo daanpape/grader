77d401e9d81ea6650fd2969b0c6ec4aaad81302a
Glenn Matthys <glenn@webmind.be>
2015-05-15 10:49:25 +0200

    Add configuration variables to control the log destination

--- dptcms/config.php
+++ dptcms/config.php
@@ -21,15 +21,25 @@
     /* Pagination settings */
     public static $pageCount	= 20;
 
-     /* Log file */
+    /**
+     * Controls the destination of the logging. Set to syslog or file.
+     * When set to file, define public static $logfile.
+     * @var string Either 'syslog' or 'file'
+     */
+    public static $logDestination = 'file';
+    /**
+     * When $logDestination is set to 'file', this variable controls which file
+     * logging goes to.
+     * @var string A filepath
+     */
      public static $logfile = './logfile.log';
      
    /*
     * Image upload settings
     */
-   public static $fileImgSupport          = array("image/gif", "image/jpeg", "image/jpg", "image/pjpeg", "image/x-png", "image/png", "application/vnd.ms-excel","text/plain", "text/csv", "text/tsv", "application/octet-stream", "");
-   public static $fileFriendlySupport     = 'GIF, JPEG, PNG, CSV';
-   public static $fileMaxSize             = 4096;
-   public static $fileDestination	  = 'upload';
+    public static $fileImgSupport          = array("image/gif", "image/jpeg", "image/jpg", "image/pjpeg", "image/x-png", "image/png", "application/vnd.ms-excel","text/plain", "text/csv", "text/tsv", "application/octet-stream", "");
+    public static $fileFriendlySupport     = 'GIF, JPEG, PNG, CSV';
+    public static $fileMaxSize             = 4096;
+    public static $fileDestination	  = 'upload';
  }
 ?>
