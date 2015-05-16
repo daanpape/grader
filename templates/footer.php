3fad4b1ae3a71a343f9a0e6f94db1bb0d17fd88e
Glenn Matthys <glenn@webmind.be>
2015-05-16 18:41:55 +0200

    Templates: Provide a common header & footer for all pages

---
+++ templates/footer.php
@@ -0,0 +1,19 @@
+<script src="/js/i18n.js"></script>
+<script src="/js/jquery.min.js"></script>
+<script src="/js/jsrapport/jquery-ui.min.js"></script>
+<script src="/js/knockout-3.3.0.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/ie10-viewport-bug-workaround.js"></script>
+<script src="/js/modals.js"></script>
+<script src="/js/global.js"></script>
+<script src="/js/<?php echo $location ?>ViewModel.js"></script>
+
+<!-- Metis Menu Plugin JavaScript -->
+<script src="/admin_static/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="/admin_static/dist/js/sb-admin-2.js"></script>
+
+</body>
+
+</html>
---
+++ templates/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html id="htmldoc">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title data-bind="text: title">Oopsie, the dev forgot a title!</title>
+    
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/grader.css" rel="stylesheet">
+    <link href="/css/cssrapport/jquery-ui.min.css" rel="stylesheet">
+    <link href="/admin_static/dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="/admin_static/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="../../css/grader.css">
+
+    <!--[if lt IE 9]>
+        <script src="/js/html5shiv.min.js"></script>
+        <script src="/js/respond.min.js"></script>
+    <![endif]-->
+
+</head>
