c05ba26ec9fbe7bccb3de2143a57afbbdcd12b09
nielsverhaegen <captustv@gmail.com>
2015-05-18 17:02:19 +0200

    documents

--- js/assesscompletementViewModel.js
+++ js/assesscompletementViewModel.js
@@ -32,7 +32,7 @@ function pageViewModel(gvm) {
     {
         $.getJSON('/api/project/'+ gvm.projectId + '/documents/' + gvm.studentId, function(data) {
             $.each(data, function(i, item) {
-                gmv.userData.push(item);
+                gvm.userData.push(item);
             });
         });
     };
