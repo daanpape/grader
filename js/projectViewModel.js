bb907bfab1d10a99e05ca34bcc796f62dff685b4
nielsverhaegen <captustv@gmail.com>
2015-05-19 15:54:44 +0200

    bug fix

--- js/projectViewModel.js
+++ js/projectViewModel.js
@@ -340,10 +340,11 @@ function totalPercentCheck()
             for(var indexIndicators = 0; indexIndicators < viewModel.competences()[indexCompetences].subcompetences()[indexSubcompetence].indicators().length; indexIndicators++)
             {
                 totalPercentIndicators[nrOfIndicators] = totalPercentIndicators[nrOfIndicators] + parseInt(viewModel.competences()[indexCompetences].subcompetences()[indexSubcompetence].indicators()[indexIndicators].weight());
+                nrOfIndicators++;
             }
-            nrOfIndicators++;
+            nrOfSubcompetences++;
         }
-        nrOfSubcompetences++;
+
     }
 
     for(var i = 0; i < nrOfSubcompetences - 1; i++)
