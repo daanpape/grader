f9c421fc6c4070ec0dfbc6d429c509c869899a0d
nielsverhaegen <captustv@gmail.com>
2015-05-19 15:46:43 +0200

    bug fix

--- js/projectViewModel.js
+++ js/projectViewModel.js
@@ -95,7 +95,7 @@ function SubCompetence(parent, id, code, name, weight, locked, indicators) {
             }
 
             self.indicators.remove(indicator);
-            console.log(self.indicators);
+            console.log(self.indicators());
             automatedWeightCalculation(this.indicators());
         }
 
