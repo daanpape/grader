9f99e7e72a71c2c8e0b834ad2243941ee9291b17
nielsverhaegen <captustv@gmail.com>
2015-05-20 18:08:15 +0200

    action bug

--- templates/projectrules.php
+++ templates/projectrules.php
@@ -45,7 +45,7 @@ $location = "projectRules";
                         </td>
 
                         <td>
-                            <select data-bind="options: viewModel.projectActions.name" class="form-control form-next">
+                            <select data-bind="options: viewModel.projectActions" class="form-control form-next">
                             </select>
                             <select data-bind="foreach: viewModel.availableOperators, value: operator" class="form-control form-next">
                                 <option data-bind="text: $data"></option>
