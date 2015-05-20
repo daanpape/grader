bc443147ffd1cf8d3a6c61c3455bf13025c6c3b1
nielsverhaegen <captustv@gmail.com>
2015-05-20 22:11:04 +0200

    bug

--- js/assesscompletementViewModel.js
+++ js/assesscompletementViewModel.js
@@ -43,7 +43,8 @@ function pageViewModel(gvm) {
             $.each(data, function(i, item) {
                 gvm.userData.push(item);
             });
-        }).done( gvm.getDocumentsToSubmit());
+            gvm.getDocumentsToSubmit()
+        });
     }
 
     gvm.getDocumentsToSubmit = function() {
