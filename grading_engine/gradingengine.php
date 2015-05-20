d6bcd5582961982a27ebe737161997444a5838bc
nielsverhaegen <captustv@gmail.com>
2015-05-20 13:48:45 +0200

    grader engine implement documents

--- grading_engine/gradingengine.php
+++ grading_engine/gradingengine.php
@@ -313,7 +313,7 @@ class GradingEngine {
                 $totalDocuments = 0;
                 foreach($documents as $document)
                 {
-                    $totalDocuments = (int)($document->submitted);
+                    $totalDocuments = (int)($document['submitted']);
                 }
                 if ($rule->operator == '<') {
                     if($totalDocuments < $rule->value)
