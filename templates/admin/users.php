f02d91b27ded47a66f41d7399ba5e8ee9b200175
Mathias <mathiasverbanck@gmail.com>
2015-06-26 14:30:16 +0200

    undo

--- templates/admin/users.php
+++ templates/admin/users.php
@@ -82,11 +82,13 @@ require_once('templates/header.php');
                         <td data-bind="text: lastname"></td>
                         <td data-bind="text: status"></td>
                         <!-- ko if: status() === 'ACTIVE' -->
-                        <td style="width: 15%" data-bind="if:status"><a style="cursor:pointer"><i class="fa fa-toggle-on fa-lg" data-bind="click: changeStatus"></i></a> <a style="cursor:pointer"><i class="fa fa-times fa-lg" data-bind="click: removeThisUser"></i></a><a style="cursor:pointer">
+                        <td style="width: 15%" data-bind="if:status"><a style="cursor:pointer"><i class="fa fa-toggle-on fa-lg" data-bind="click: changeStatus"></i></a>
+                            <a style="cursor:pointer"><i class="fa fa-times fa-lg" data-bind="click: removeThisUser"></i></a><a style="cursor:pointer">
                                 <i class="fa fa-archive fa-lg" data-bind="attr:{'href': '/api/adminUsersCourse/'}"></td>
                         <!-- /ko -->
                         <!-- ko if: status() !== 'ACTIVE' -->
-                        <td style="width: 15%" data-bind="if:status"><a style="cursor:pointer"><i class="fa fa-toggle-off fa-lg" data-bind="click: changeStatus"></i></a> <a style="cursor:pointer"><i class="fa fa-times fa-lg" data-bind="click: removeThisUser"></i></a><a style="cursor:pointer">
+                        <td style="width: 15%" data-bind="if:status"><a style="cursor:pointer"><i class="fa fa-toggle-off fa-lg" data-bind="click: changeStatus"></i></a>
+                            <a style="cursor:pointer"><i class="fa fa-times fa-lg" data-bind="click: removeThisUser"></i></a><a style="cursor:pointer">
                                 <i class="fa fa-archive fa-lg" data-bind="attr:{'href': '/api/adminUsersCourse/'}"></td>
                         <!-- /ko -->
 
