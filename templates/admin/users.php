9bc04f77f2ee9bd0271d240b4e7e359d5fac849f
Mathias <mathiasverbanck@gmail.com>
2015-06-26 14:18:05 +0200

    page declareren

--- templates/admin/users.php
+++ templates/admin/users.php
@@ -83,12 +83,12 @@ require_once('templates/header.php');
                         <td data-bind="text: status"></td>
                         <!-- ko if: status() === 'ACTIVE' -->
                         <td style="width: 15%" data-bind="if:status"><a style="cursor:pointer"><i class="fa fa-toggle-on fa-lg" data-bind="click: changeStatus"></i></a> <a style="cursor:pointer"><i class="fa fa-times fa-lg" data-bind="click: removeThisUser"></i></a><a style="cursor:pointer">
-                                <i class="fa fa-archive fa-lg" data-bind="attr:{'href': '/api/coursemodule/' + tid + '/' + tname}"></td>
-
+                                <i class="fa fa-archive fa-lg" data-bind="attr:{'href': '/api/adminUsersCourse/'}"></td>
+                        
                         <!-- /ko -->
                         <!-- ko if: status() !== 'ACTIVE' -->
                         <td style="width: 15%" data-bind="if:status"><a style="cursor:pointer"><i class="fa fa-toggle-off fa-lg" data-bind="click: changeStatus"></i></a> <a style="cursor:pointer"><i class="fa fa-times fa-lg" data-bind="click: removeThisUser"></i></a><a style="cursor:pointer">
-                                <i class="fa fa-archive fa-lg" data-bind="attr:{'href': '/api/coursemodule/' + tid + '/' + tname}"></td>
+                                <i class="fa fa-archive fa-lg" data-bind="attr:{'href': '/api/adminUsersCourse/'}"></td>
                         <!-- /ko -->
 
                     </tr>
