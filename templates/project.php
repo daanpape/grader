fb854f71bbdeb1d403bc846cf510b381262f2898
nielsverhaegen <captustv@gmail.com>
2015-09-24 13:35:56 +0200

    better ui

--- templates/project.php
+++ templates/project.php
@@ -106,7 +106,7 @@ $location = "project";
                         Add competence
                     </button>
                     
-                    <button class="btn btn-lg savePageBtn pull-right" data-bind="text: savePage">
+                    <button class="btn btn-lg savePageBtn pull-right" data-bind="text: nextPage">
                         Save
                     </button>
                 </div>
