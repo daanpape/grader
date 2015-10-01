056177cc745739e2e8411cca6164009f4741376e
Mathias <mathiasverbanck@gmail.com>
2015-10-01 09:34:01 +0200

    manageStudentPage Maken             courses ophalen compleet

--- js/jsrapport/studentmanagementrapportenViewModel.js
+++ js/jsrapport/studentmanagementrapportenViewModel.js
@@ -31,10 +31,79 @@ function getAllStudents() {
     return students;
 }
 
+/*
+* Load page of the table
+*/
+function loadTablePage(pagenr)
+{
+    $.getJSON('/api/coursesrapport/page/' + pagenr, function(data){
+
+        /* Clear current table page */
+        viewModel.clearTable();
+
+        // Load table data
+        $.each(data.data, function(i, item) {
+            viewModel.addTableData(item.id, item.code, item.name, item.description);
+        });
+
+        /* Let previous en next buttons work */
+        if(data.prev == "none"){
+            $('#pager-prev-btn').addClass('disabled');
+        } else {
+            $('#pager-prev-btn').removeClass('disabled');
+            $('#pager-prev-btn a').click(function(){
+                loadTablePage(data.prev);
+            });
+        }
+
+        if(data.next == "none"){
+            $('#pager-next-btn').addClass('disabled');
+        } else {
+            $('#pager-next-btn').removeClass('disabled');
+            $('#pager-next-btn a').click(function(){
+                loadTablePage(data.next);
+            });
+        }
+
+        // Number of pager buttons
+        var numItems = $('.pager-nr-btn').length;
+
+        /* Calculate for the pager buttons */
+        var lowPage = Math.floor(pagenr/numItems) + 1;
+
+        $('.pager-nr-btn').each(function() {
+            /* calculate current page number */
+            var thispagenr = lowPage++;
+
+            /* Add the page number */
+            $(this).html('<a href="#">' + thispagenr + '</a>');
+
+            /* Add active class to current page */
+            if(thispagenr == pagenr) {
+                $(this).addClass('active');
+            } else {
+                $(this).removeClass('active');
+            }
+
+            /* Disable inactive classes and bind handlers to active classes */
+            if(thispagenr > data.pagecount) {
+                $(this).addClass('disabled');
+            } else {
+                /* Add click listener for button */
+                $(this).click(function() {
+                    loadTablePage(thispagenr);
+                });
+            }
+        });
+    });
+}
+
 function initPage() {
     $.getJSON('/api/currentuser', function(data) {
         viewModel.userId = data.id;
     });
 
-        $('#studentsComplete').autocomplete({ source: getAllStudents() });
+    $('#studentsComplete').autocomplete({ source: getAllStudents() });
+
+    loadTablePage(1);
 }
