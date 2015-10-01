99ffd0c8bc7afd2829754a31760892bfc7818a7e
Mathias <mathiasverbanck@gmail.com>
2015-10-01 09:17:05 +0200

    manageStudentPage Maken studenten ophalen test

--- js/jsrapport/studentmanagementrapportenViewModel.js
+++ js/jsrapport/studentmanagementrapportenViewModel.js
@@ -1,46 +1,238 @@
 var userid;
 //arrays gebruikt voor autocompletes en selecteren waarde hiervan.
-var students = [];
-var studentsid = [];
+var studentLists = [];
+var studentListsid = [];
+var teachers = []
+var teachersid = []
 
-// View model for the page
 function pageViewModel(gvm) {
-    // Page specific i18n bindings
-    gvm.app  = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("WorksheetNameRapport");}, gvm);
-    gvm.title = ko.computed(function (){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("HomeRapportTitle");}, gvm);
-    gvm.projectname = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("ProjectRapportName");}, gvm);
-    gvm.homeManual = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("WorksheetRapportManual");}, gvm);
+    gvm.projecttitle = ko.observable("");
+    gvm.userId = -1;
+    gvm.coupledCount = 0;
 
+    // Page specific i18n bindings
+    gvm.title = ko.computed(function (){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("ProjectTitle") + ": " + gvm.projecttitle();}, gvm);
+    gvm.pageHeader = ko.observable("Project");
+    gvm.projectname = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("ProjectName");}, gvm);
+
+    gvm.addBtn = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("AddBtn")}, gvm);
+
+    // The table data observable array
     gvm.tabledata = ko.observableArray([]);
 
-    // Table i18n bindings
-    gvm.courseID = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("courseID");}, gvm);
-    gvm.courseName = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("courseName");}, gvm);
-    gvm.volgStatus = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("volgStatus");}, gvm);
-    gvm.courseAction = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("courseAction");}, gvm);
+    // Add data to the table
+    gvm.addTableData = function(studid, userid, studlist, name) {
+        // Push data
+        var tblOject = {tid: studid + "-" + userid, tstudlist: studlist, tteacher: name};
+        gvm.tabledata.push(tblOject);
 
-    //get all students
-    function getAllStudents() {
-        students = [];
-        studentsid = [];
-        $.getJSON('/api/allstudents', function(data) {
-            $.each(data, function(i, item) {
-                students.push(item.firstname + " " + item.lastname);
-                studentsid.push(item.id);
+        // Attach delete handler to delete button
+        $('#removebtn-' + studid + "-" + userid).bind('click', function(event, data){
+            // Delete the table item
+            deleteTableItem(studid + "-" + userid, tblOject);
+            event.stopPropagation();
+        });
+    }
+
+    gvm.clearTable = function() {
+        gvm.tabledata.removeAll();
+    }
+
+}
+
+function getAllTeachers() {
+    teachers = [];
+    teachersid = [];
+    $.getJSON('/api/getteacherrapport', function(data) {
+        $.each(data, function(i, item) {
+            teachers.push(item.firstname + " " + item.lastname);
+            teachersid.push(item.id);
+        });
+    });
+    return teachers;
+}
+
+function getAllStudentLists() {
+    studentLists = [];
+    studentListsid = [];
+    $.getJSON('/api/studentlistsrapporten/' + userid, function (data) {
+        $.each(data, function (i, item) {
+            studentLists.push(item.name);
+            studentListsid.push(item.id);
+        });
+    });
+    return studentLists;
+}
+
+function getTeacherid() {
+
+    // functie voor het opsplitsen van naam indien gewenst.
+    // console.log(getTeacherID($('#teachersComplete').val().substr(0,$('#teachersComplete').val().indexOf(' ')), $('#teachersComplete').val().substr($('#teachersComplete').val().indexOf(' ')+1)));
+
+    var i = 0;
+    var teacher = 0;
+    teachers.forEach(function(entry) {
+        if (new String(entry).valueOf() == new String($('#teachersComplete').val()).valueOf()) {
+            teacher = teachersid[i];
+        }
+        i+= 1;
+    });
+    return teacher
+}
+
+function getGroupid() {
+    var i = 0;
+    var studlijst;
+    studentLists.forEach(function(entry) {
+        if (new String(entry).valueOf() == new String($('#studentListComplete').val()).valueOf()) {
+            studlijst = (studentListsid[i]);
+        }
+        i+= 1;
+    });
+    return studlijst;
+}
+
+function addGroup(courseid, teacherid, studlijstid) {
+    //TODO if teacher or studlijst = 0 dan bestaat deze niet!
+    //TODO momenteel niet mogelijk om meer als 1 maal zelfde velden in te voeren maar geen foutboodschap
+
+    $.ajax({
+        url: "/api/coursecouple/" + courseid + "/" + studlijstid + "/" + teacherid,
+        type: "POST",
+        data: {'course': courseid, 'teacher': teacherid, 'studentlist': teacherid},
+        success: function(data) {
+            loadTablePage(1,courseid);
+            //callback(true);
+        },
+        error: function(data) {
+            console.log('Failed to add new student');
+            //callback(false);
+        }
+    });
+}
+
+/*
+ * Load page of the table
+ */
+function loadTablePage(pagenr,course)
+{
+    $.getJSON('/api/getStudentGroupTeacherByCourseID/page/' + pagenr + '/' + course, function(data){
+
+        /* Clear current table page */
+        viewModel.clearTable();
+
+        // Load table data
+        $.each(data.data, function(i, item) {
+
+            viewModel.addTableData(item.studid, item.userid , item.name , item.firstname + " " + item.lastname, item.id);
+
+
+        });
+
+        //TODO pagers doen werken
+        //Momenteel wordt enkel alles geselecteerd met LIMIT 0,20
+        //Maar de pagers zelf blijven dissabled waardoor het ook niet mogelijk is LIMIT 21,40 op te vragen.
+        /* Let previous en next buttons work */
+
+
+        if(data.prev == "none"){
+            $('#pager-prev-btn').addClass('disabled');
+        } else {
+            $('#pager-prev-btn').removeClass('disabled');
+            $('#pager-prev-btn a').click(function(){
+                loadTablePage(data.prev);
             });
+        }
+
+        if (data.next == "none"){
+            $('#pager-next-btn').addClass('disabled');
+        } else {
+            $('#pager-next-btn').removeClass('disabled');
+            $('#pager-next-btn a').click(function(){
+                loadTablePage(data.next);
+            });
+        }
+
+        // Number of pager buttons
+        var numItems = $('.pager-nr-btn').length;
+
+
+        /* Calculate for the pager buttons */
+        var lowPage = Math.floor(pagenr/numItems) + 1;
+
+
+        $('.pager-nr-btn').each(function() {
+            /* calculate current page number */
+            var thispagenr = lowPage++;
+
+            /* Add the page number */
+            $(this).html('<a href="#">' + thispagenr + '</a>');
+
+            /* Add active class to current page */
+            if(thispagenr == pagenr) {
+                $(this).addClass('active');
+            } else {
+                $(this).removeClass('active');
+            }
+
+            /* Disable inactive classes and bind handlers to active classes */
+            if(thispagenr > data.pagecount) {
+                $(this).addClass('disabled');
+            } else {
+                /* Add click listener for button */
+                $(this).click(function() {
+                    loadTablePage(thispagenr, course);
+                });
+            }
         });
-        return students;
-    }
+    });
+}
 
-    function initPage() {
-        console.log("loadpage");
-        $("#errormessage").hide();
+/*
+ * Delete item from table given the id.
+ */
+function deleteTableItem(id, tblOject) {
+    showYesNoModal("Bent u zeker dat u dit item wil verwijderen? \r\n Let op: verwijderde items blijven in het systeem en kunnen weer actief gezet worden door een administrator. \r\n Gelieve de administrator te contacteren om een vak definitief te verwijderen.", function(val){
+        if(val){
+            //courseid = $('#projectHeader').attr("data-value");
+            //studielijstid = "studid " + id.substr(0,id.indexOf('-'));
+            //teacherid = "userid " + id.substr(id.indexOf('-')+1);
 
-        $('#studentsComplete').autocomplete({ source: getAllStudents() });
+            $.ajax({
+                url: "/api/setInactiveCourseStudlistCouple/" + $('#projectHeader').attr("data-value") + '/' + id.substr(0,id.indexOf('-')) + '/' + id.substr(id.indexOf('-')+1),
+                type: "DELETE",
+                success: function() {
+                    viewModel.tabledata.remove(tblOject);
+                }
+            });
 
-        $.getJSON('/api/currentuser', function (data) {
-            viewModel.userId = data.id;
-            viewModel.updateCourseRapport();
-        });
-    }
+        }
+    });
+}
+
+function initPage() {
+    $('#addGroupForm').hide();
+
+    $('#addCoursemembers').click(function() {
+        $("#addGroupForm").show();
+        $('#teachersComplete').autocomplete({ source: getAllTeachers() });
+        $('#studentListComplete').autocomplete({ source: getAllStudentLists() });
+    });
+
+    $('#addGroupBtn').click(function() {
+        addGroup($('#projectHeader').attr("data-value"), getTeacherid(), getGroupid());
+
+        $('#studentListComplete').val("");
+        $('#teachersComplete').val("");
+
+        loadTablePage(1,$('#projectHeader').attr("data-value"));
+
+        $('addGroupForm').hide();
+    });
+
+    $.getJSON('/api/currentuser', function(data) {
+        viewModel.userId = data.id;
+        userid = data.id;
+    });
+    loadTablePage(1,$('#projectHeader').attr("data-value"));
 }
--- templates/templatesrapport/studentmanagementrapporten.php
+++ templates/templatesrapport/studentmanagementrapporten.php
@@ -1,11 +1,10 @@
 <?php
 $location = "jsrapport/studentmanagementrapporten";
 ?>
-
 <!DOCTYPE html>
 <html lang="nl" id="htmldoc">
 <head>
-    <?php include_once('templates/hddepends.php'); ?>
+    <?php include_once('templates/hddepends.php') ?>
 </head>
 
 <body>
@@ -13,56 +12,73 @@ $location = "jsrapport/studentmanagementrapporten";
 
 <!-- Header container -->
 <div class="container">
-    <h1 class="page-header" data-bind="text: app">Student Management</h1>
+    <h1 class="page-header" id="projectHeader" data-value="<?php echo $coursestudentsid ?>"><?php echo $coursestudentsname ?></h1>
 </div>
 
 <!-- Content container -->
 <div class="container">
     <div class="row">
-        <div class="dropdown col-md-4">
-            <label>Student name:</label> <input id="studentsComplete" name="studentname" class="coursesInputField" />
+        <div class="col-md-12">
+            <table class="table table-striped">
+                <thead>
+                <!-- TODO data-bind aanmaken
+
+                <tr>
+                    <th data-bind="text: studlistTableTitle">StudentList</th>
+                    <th data-bind="text: teacherTableTitle">Teachers</th>
+                    <th data-bind="text: actionTableTitle">Actions</th>
+                </tr>
+                -->
+                <tr>
+                    <th>StudentList</th>
+                    <th>Teachers</th>
+                    <th>Actions</th>
+                </tr>
+
+                </thead>
+                <tbody data-bind="foreach: tabledata">
+                <tr>
+                    <td data-bind="text: tstudlist">--</td>
+                    <td data-bind="text: tteacher">--</td>
+                    <td>
+                        <span class="glyphicon glyphicon-trash glyphicon-btn" data-bind="attr:{'id': 'removebtn-' + tid}"></span>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <!-- Pagination and action container -->
+        <div class="container">
+            <!-- Pagination -->
+            <ul class="pagination float_left">
+                <li id="pager-prev-btn"><a href="#" >&laquo;</a></li>
+                <li class="pager-nr-btn"><a href="#" >1</a></li>
+                <li class="pager-nr-btn"><a href="#" >2</a></li>
+                <li class="pager-nr-btn"><a href="#">3</a></li>
+                <li class="pager-nr-btn"><a href="#">4</a></li>
+                <li class="pager-nr-btn"><a href="#">5</a></li>
+                <li id="pager-next-btn"><a href="#" >&raquo;</a></li>
+            </ul>
+
+            <button type="button" class="btn btn-default pagination-button" id="addCoursemembers">
+                <span class="glyphicon glyphicon-plus"></span> <span data-bind="text: addBtn"></span>
+            </button>
+        </div>
+
+    </div>
+    <div id="addGroupForm" class="container">
+        <div class="row" >
+            <label>Teacher name:</label> <input id="teachersComplete" name="teachername" class="coursesInputField" />
+            <br />
+            <label>StudentList:</label> <input id="studentListComplete" name="studentlistname" class="coursesInputField"/>
+            <br />
+            <button id="addGroupBtn" class="btn btn-default">Add</button>
         </div>
     </div>
 </div>
 
-<!-- Content container -->
-<div class="container">
-    <table class="table table-striped">
-        <thead>
-        <tr>
-            <th data-bind="text: courseID">Course ID</th>
-            <th data-bind="text: courseName">Name</th>
-            <th data-bind="text: volgStatus">Status</th>
-            <th data-bind="text: courseAction">Actions</th>
-        </tr>
-        </thead>
-        <tbody data-bind="foreach: tabledata">
-        <tr>
-            <td data-bind="text: tid">--</td>
-            <td data-bind="text: tname">--</td>
-            <td data-bind="text: tname">--</td>
-            <td>
-                <span class="glyphicon glyphicon-pencil glyphicon-btn" data-bind="attr:{'id': 'editbtn-' + tid}"></span>
-                <span class="glyphicon glyphicon-copyright-mark glyphicon-btn" data-bind="attr:{'id': 'copybtn-' + tid}"></span>
-                <span class="glyphicon glyphicon-trash glyphicon-btn" data-bind="attr:{'id': 'removebtn-' + tid}"></span>
-            </td>
-        </tr>
-        </tbody>
-    </table>
-</div>
 
-<div class="container">
-    <!-- Pagination -->
-    <ul class="pagination float_left">
-        <li id="pager-prev-btn"><a href="#" >&laquo;</a></li>
-        <li class="pager-nr-btn"><a href="#" >1</a></li>
-        <li class="pager-nr-btn"><a href="#" >2</a></li>
-        <li class="pager-nr-btn"><a href="#">3</a></li>
-        <li class="pager-nr-btn"><a href="#">4</a></li>
-        <li class="pager-nr-btn"><a href="#">5</a></li>
-        <li id="pager-next-btn"><a href="#" >&raquo;</a></li>
-    </ul>
-</div>
 
 <?php include_once('templates/jsdepends.php') ?>
 </body>
