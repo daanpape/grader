6014d6a03144dac41bb58a95d65e9b4ad3227f8e
Mathias <mathiasverbanck@gmail.com>
2015-10-01 09:35:33 +0200

    manageStudentPage Maken             courses ophalen compleet

--- js/jsrapport/studentmanagementrapportenViewModel.js
+++ js/jsrapport/studentmanagementrapportenViewModel.js
@@ -43,7 +43,7 @@ function loadTablePage(pagenr)
 
         // Load table data
         $.each(data.data, function(i, item) {
-            viewModel.addTableData(item.id, item.code, item.name, item.description);
+            viewModel.addTableData(item.id, item.code);
         });
 
         /* Let previous en next buttons work */
