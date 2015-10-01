ddc73e5236225e91475f505c6973458ed139f5f2
Mathias <mathiasverbanck@gmail.com>
2015-10-01 09:24:56 +0200

    manageStudentPage Maken                  studenten ophalen                  test2

--- js/jsrapport/studentmanagementrapportenViewModel.js
+++ js/jsrapport/studentmanagementrapportenViewModel.js
@@ -3,163 +3,19 @@ var worksheetsid = [];
 
 //viewmodel for the assess page
 function pageViewModel(gvm) {
-    gvm.app  = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("ProjectRapportName")}, gvm);
-    gvm.title = ko.computed(function(){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n. __("AssessTitle2");}, gvm);
-    gvm.userId = -1;
-
     // Page specific i18n bindings
-    gvm.pageHeader = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("AssessTitle2");}, gvm);
+    gvm.app  = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("WorksheetNameRapport");}, gvm);
+    gvm.title = ko.computed(function (){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("HomeRapportTitle");}, gvm);
     gvm.projectname = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("ProjectRapportName");}, gvm);
-    gvm.homeManual = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("HomeRapportManual");}, gvm);
-    gvm.selectCourse = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("SelectCourse");}, gvm);
+    gvm.homeManual = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("WorksheetRapportManual");}, gvm);
 
-    // Pagination i18n bindings
-    gvm.addBtn = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("AddBtn")}, gvm);
-
-    // Table i18n bindings
-    gvm.werkficheID = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("werkficheID");}, gvm);
-    gvm.werkficheName = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("werkficheName");}, gvm);
-    gvm.werkficheDate = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("werkficheDate");}, gvm);
-    gvm.werkficheAction = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("werkficheAction");}, gvm);
-
-    gvm.availableCourses = ko.observableArray([]);
-    gvm.availableStudentlists = ko.observableArray([]);
-    gvm.availableStudents = ko.observableArray([]);
-
-    gvm.currentCourseId = null;
-    gvm.currentStudentlistId = null;
-    gvm.currentStudentId = null;
-
-    gvm.updateDropdowns = function() {
-        $.getJSON('api/lastdropdownrapporten/' + gvm.userId, function(data) {
-            if(!$.isEmptyObject(data)) {
-                $.each(data, function(i, item) {
-                    $(".btn-courseRapport span:first").text(item.course);
-                    $(".btn-studentlist span:first").text(item.studentlist);
-                    $('.btn-student span:first').text(item.student);
-                    gvm.currentCourseId = item.courseid;
-                    gvm.updateStudentlists(item.courseid, gvm.userId);
-                    gvm.currentStudentlistId = item.studentlistid;
-                    gvm.updateStudents(item.studentlistid);
-                    gvm.currentStudentId = item.studentid;
-                    gvm.updateCourseRapport();
-                    loadTablePage(1, gvm.currentCourseId);
-                });
-            } else {
-                gvm.updateCourseRapport();
-            }
-        });
-    }
-
-    //Houdt bij wat geselecteerd wordt
-    //Wordt opgeroepen bij iedere wijziging (niet ingevulde velden = NULL)
-    gvm.saveLastSelectedDropdowns = function() {
-        data = {};
-        data["user"] = gvm.userId;
-        data["course"] = $(".btn-courseRapport span:first").text();
-        data["courseid"] = gvm.currentCourseId;
-        data["studentlist"] = $('.btn-studentlist span:first').text();
-        data["studentlistid"] = gvm.currentStudentlistId;
-        data["student"] = $(".btn-student span:first").text();
-        data["studentid"] = gvm.currentStudentId;
-        $.ajax({
-            type: "POST",
-            url: "/api/savedropdownsRapport",
-            data: data,
-            success: function() {
-                console.log("success");
-            }
-        })
-    }
-
-    gvm.updateCourseRapport = function() {
-        $.getJSON('/api/coursefromteacher/' + gvm.userId, function(data) {
-            gvm.availableCourses.removeAll();
-            $.each(data, function(i, item) {
-                //  Put item in list
-                gvm.availableCourses.push(item);
-
-                // Add listener to listitem
-                $("#coursebtn-" + item.id).click(function(){
-                    gvm.currentCourseId = item.id;
-                    gvm.currentStudentlistId = null;
-                    gvm.currentStudentId = null;
-                    gvm.updateStudentlists(item.id, gvm.userId);
-                    $(".btn-courseRapport span:first").text($(this).text());
-                    $(".btn-studentlist span:first").text("Studentlist");
-                    $('.btn-student span:first').text("Student");
-                    gvm.saveLastSelectedDropdowns();
-                });
-            });
-        });
-    };
-
-    /*
-     * Update the student data
-     */
-    gvm.updateStudentlists = function(cid, uid) {
-        //get all studentlists
-        $.getJSON('api/studentlistdrop/' + cid + '/' + uid, function(data) {
-            gvm.availableStudentlists.removeAll();
-            $.each(data, function(i,item) {
-                // Put item in list
-                gvm.availableStudentlists.push(item);
-
-                // Add listener to listitem
-                $("#studentlistbtn-" + item.id).click(function(){
-                    gvm.currentStudentlistId = item.id;
-                    gvm.currentStudentId = null;
-                    gvm.updateStudents(item.id);
-                    $(".btn-studentlist span:first").text($(this).text());
-                    $('.btn-student span:first').text("Student");
-                    gvm.saveLastSelectedDropdowns();
-                });
-            })
-        })
-    }
-
-
-    /*
-     * Update the student data
-     */
-    gvm.updateStudents = function(id) {
-        //get all students who follow that course
-        $.getJSON('api/studentdrop/' + id, function(data) {
-            gvm.availableStudents.removeAll();
-            $.each(data, function(i,item) {
-                // Put item in list
-                gvm.availableStudents.push(item);
-
-                // Add listener to listitem
-                $("#studentbtn-" + item.id).click(function(){
-                    gvm.currentStudentId = item.id;
-                    $(".btn-student span:first").text($(this).text());
-                    gvm.saveLastSelectedDropdowns();
-                    loadTablePage(1,gvm.currentCourseId);
-                });
-            })
-        })
-    }
-
-    // The table data observable array
     gvm.tabledata = ko.observableArray([]);
 
-    // Add data to the table
-    gvm.addTableData = function(id, Name , datum, score) {
-        // Push data
-        var tblObject = {tworkid: id, tname: Name, tdatum: datum, tscore: score};
-        gvm.tabledata.push(tblObject);
-
-        $('#removebtn-' + id).bind('click', function(event, data){
-            // Delete the table item
-            deleteTableItem(id, gvm.currentStudentId, tblObject);
-            event.stopPropagation();
-        });
-    }
-
-    gvm.clearTable = function() {
-        gvm.tabledata.removeAll();
-    }
+    // Table i18n bindings
+    gvm.courseID = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("courseID");}, gvm);
+    gvm.courseName = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("courseName");}, gvm);
+    gvm.volgStatus = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("volgStatus");}, gvm);
+    gvm.courseAction = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("courseAction");}, gvm);
 }
 
 //get all students
@@ -253,27 +109,8 @@ function initPage() {
         viewModel.userId = data.id;
         viewModel.updateDropdowns();
     });
-
-
-
+    
         $('#studentsComplete').autocomplete({ source: getAllWorksheets() });
 
 
-    $('#addNewWorksheetBtn').click(function() {
-        //TODO controle of er effectief een lijst / student geselecteerd is.
-
-        //controleren of het voor 1 student is of voor alle studenten
-        if ($("input:radio[name='studenten']:checked").val() == "student") { //student
-            addWorksheetStudent(getWorksheetid());
-        }
-        else { //studentlist
-            addWorksheetStudentList(getWorksheetid());
-        }
-        //table opnieuw laden
-        loadTablePage(1,viewModel.currentCourseId);
-
-        //Indien gewenst toevoegformulier weer verbergen.
-        $('#worksheetComplete').val("");
-
-    });
 }
