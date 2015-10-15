48fe65df1d4cc529b47f823b804f468f1811d415
nielsverhaegen <captustv@gmail.com>
2015-10-15 14:49:08 +0200

    student list

--- templates/assessproject.php
+++ templates/assessproject.php
@@ -62,10 +62,10 @@ $location = "assessproject";
                 <tr>
                     <td date-bind="text: tcountAssessed"></td>
                     <!-- ko if: tcountAssessed == 0 -->
-                    <td><img src="../img/check_red.png" alt="" title="red"></td>
+                    <td style="text-align: center"><img src="../img/check_red.png" alt="" title="red"></td>
                     <!-- /ko -->
                     <!-- ko if: tcountAssessed > 0 -->
-                    <td><img src="../img/check_orange.png" alt="" title="orange"></td>
+                    <td style="text-align: center"><img src="../img/check_orange.png" alt="" title="orange"></td>
                     <!-- /ko -->
 
                     <td data-bind="text: tfirstname">--</td>
