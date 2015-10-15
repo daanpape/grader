6e1748de37ffe53c2abe6360770d4fc2defe69fd
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-10-15 11:33:24 +0200

    better ui projects

--- templates/assessproject.php
+++ templates/assessproject.php
@@ -60,13 +60,10 @@ $location = "assessproject";
                 </thead>
                 <tbody data-bind="foreach: tabledata">
                 <tr>
-                    <!-- ko if: tcountAssessed === 0 -->
+                    <!-- ko if: tcountAssessed == 0 -->
                     <td><img src="../img/check_red.png" alt="" title="red"></td>
                     <!-- /ko -->
                     <!-- ko if: tcountAssessed > 0 -->
-                    <td><img src="../img/check_orange.png" alt="" title="orange"></td>
-                    <!-- /ko -->
-                    <!-- ko if: 1 === 1 -->
                     <td><img src="../img/check_orange.png" alt="" title="orange"></td>
                     <!-- /ko -->
 
