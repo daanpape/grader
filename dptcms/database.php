d52c9a783fa8b981223cb75f3f767220f5548fde
nielsverhaegen <captustv@gmail.com>
2015-10-15 14:38:02 +0200

    student list

--- dptcms/database.php
+++ dptcms/database.php
@@ -1412,7 +1412,7 @@ class UserDAO {
                 $stmt = $conn->prepare("SELECT id,firstname,lastname,mail FROM students WHERE id = ?");
                 $stmt->execute(array($student['student']));
                 $data = $stmt->fetchAll();
-                $studentData->id = 'Data';
+                $studentData->id = $data.id;
                 array_push($returnData,$studentData);
             }
 
