40839f38942b1fc6ffcc05dac441a9a564ef8bdc
nielsverhaegen <captustv@gmail.com>
2015-10-22 14:05:12 +0200

    search

--- js/assessprojectViewModel.js
+++ js/assessprojectViewModel.js
@@ -112,7 +112,7 @@ function createPDF(id,name,lastname,email, projectheader, projectdescription)
 function getStudentByName(){
 
     if(viewModel.tempTableData.length == 0) {
-        viewModel.tempTableData = viewModel.tabledata;
+        viewModel.tempTableData = viewModel.tabledata.slice(0);
     }
     else
     {
