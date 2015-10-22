02b109dabae33c0ce86e1525c8ff75745e89b44a
nielsverhaegen <captustv@gmail.com>
2015-10-22 13:28:56 +0200

    search

--- js/assessprojectViewModel.js
+++ js/assessprojectViewModel.js
@@ -133,6 +133,7 @@ function getStudentByName(){
     viewModel.tabledata = ko.observableArray([]);
     viewModel.tabledata = data;
     console.log(viewModel.tabledata.length);
+    viewModel.tabledata.refresh();
 }
 
 function getData(id)
