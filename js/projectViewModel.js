43e87e4f876ca4ffedfcafd3e72f7755d308bf48
nielsverhaegen <captustv@gmail.com>
2015-11-12 13:36:35 +0100

    min max

--- js/projectViewModel.js
+++ js/projectViewModel.js
@@ -199,7 +199,7 @@ function fetchProjectStructure() {
                
                $.each(subcomp.indicators, function(i, indic){
                    if(indic.id > 0) {
-                       var test = 'Ja/Nee';
+                       var test = 'Punten';
                        subcompetence.indicators.push(new Indicator(subcompetence, indic.id, indic.name, indic.weight, indic.description,false,test));
                        console.log(test);
                    }
