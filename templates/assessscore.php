f87aeb8930ccd0eea88d45be7585ed72827c3a82
nielsverhaegen <captustv@gmail.com>
2015-11-13 15:01:52 +0100

    point type

--- templates/assessscore.php
+++ templates/assessscore.php
@@ -150,7 +150,8 @@ $location = "assessscore";
                                     <!-- /ko -->
 
                                     <!-- ko if: pointType() === 'Ja/Nee' -->
-                                    <input type="radio" name="ja_nee" value="100">Ja
+                                    <br>
+                                    <input type="radio" name="ja_nee" value="100">Ja<br>
                                     <input type="radio" name="ja_nee" value="0">Nee
                                     <!-- /ko -->
 
