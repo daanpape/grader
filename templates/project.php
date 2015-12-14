349f53cca0c5153ebd25474cfedb96adbf74cc6a
Niels Verhaegen <captustv@gmail.com>
2015-12-14 14:57:31 +0100

    Clean UI

--- templates/project.php
+++ templates/project.php
@@ -95,7 +95,6 @@ $location = "project";
                                             <span>Current weight: <input type="text" class="form-control form-next" data-bind="value: weight">%
                                             <i class="icon-large icon-unlock" style="vertical-align: middle;" data-bind="click: toggleLock"></i></span>
                                             <button class="btn" data-bind="click: removeThis">Remove this indicator</button>
-                                            <select data-bind="options: viewModel.availableTypes, value: pointType"></select>
                                             <div class="dropdown">
                                                 <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown"><span data-bind="text: pointType"></span>
                                                     <span class="caret"></span></button>
