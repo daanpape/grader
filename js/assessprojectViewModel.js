116553c3224c313a9a70acc247b71ff76c5ffa59
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-15 16:40:41 +0100

    nr of assessing color balls

--- js/assessprojectViewModel.js
+++ js/assessprojectViewModel.js
@@ -159,7 +159,7 @@ function getData(id)
 function getNrOfAssessing(){
     $.getJSON('/api/project/nrOfAssessing/' + viewModel.projectId , function(data)
     {
-        console.log(data["nrOfAssessing"]);
+        console.log(data[0]);
 
         return data[0];
     });
