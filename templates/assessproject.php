ffda3b850d87a7206a8a6141176c3a754a492189
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-15 16:21:00 +0100

    nr of assessing color balls

--- templates/assessproject.php
+++ templates/assessproject.php
@@ -81,6 +81,9 @@ $location = "assessproject";
                     <!-- ko if: tcountAssessed > 0-->
                     <td style="text-align: center"><img src="../img/check_orange.png" alt="" title="orange" width="25px" height="25px"></td>
                     <!-- /ko -->
+                    <!-- ko if: tcountAssessed == getNrOfAssessing() -->
+                    <td style="text-align: center"><img src="../img/check_green.png" alt="" title="orange" width="25px" height="25px"></td>
+                    <!-- /ko -->
 
 
                     <td data-bind="text: tfirstname">--</td>
