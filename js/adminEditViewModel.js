cd785de7aca2d4e6044daeaa958bb7f0bc77432f
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-15 15:39:36 +0100

    Load current permission user edit

--- js/adminEditViewModel.js
+++ js/adminEditViewModel.js
@@ -18,7 +18,7 @@ function pageViewModel(gvm) {
     gvm.permissionDescription = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("PermissionDescription");}, gvm);
 
     gvm.currentUserRole = ko.observable();
-    gvm.oldUserRole = ko.observable();
+    gvm.oldUserRole = ko.observable("GUEST");
     gvm.availablePermissions = ko.observableArray(['GUEST', 'STUDENT', 'USER', 'SUPERUSER']);
 
     gvm.rights = ko.observableArray([]);
