8dcae7778d0bda389ee387ec2b0cd784dc12472d
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-15 14:02:08 +0100

    Admin - Edit user - Save alert

--- js/adminEditViewModel.js
+++ js/adminEditViewModel.js
@@ -140,7 +140,7 @@ function saveUserPermissions(){
         url: "/api/addrole/" + viewModel.edituserid,
         data: { 'permissions': permissions },
         success: function() {
-            alert('Changes saved correctly')
+            console.log('Changes saved correctly')
         },
         error: function() {
             console.log('Error saving user permission: ' + permissions);
