08cc0d825687a060ab49eeb217a84c8039e83b98
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-15 17:08:26 +0100

    nr of assessing color balls

--- js/assessprojectViewModel.js
+++ js/assessprojectViewModel.js
@@ -63,6 +63,7 @@ function pageViewModel(gvm) {
     gvm.addTableData = function(id, firstname, lastname, email, countAssessed) {
         // Push data
         var data = countAssessed;
+        console.log(data);
         var tblOject = {tid: id, tfirstname: firstname, tlastname: lastname, tScoreTableBtn: gvm.scoreTableTitle, tFilesTableBtn: gvm.filesTableTitle, tpid: gvm.projectId, email: email, tcountAssessed: data};
         gvm.tabledata.push(tblOject);
     };
@@ -162,7 +163,7 @@ function getNrOfAssessing(){
         data.forEach(function(item)
         {
             console.log(item.nrOfAssessing);
-            if (viewModel.tcountAssessed() == item.nrOfAssessing){
+            if (viewModel.tcountAssessed([]) == item.nrOfAssessing){
                 alert("test");
             }
 
