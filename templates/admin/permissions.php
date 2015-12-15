952c682157d1ea14bf2a225526b19621a49107ad
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-15 14:22:09 +0100

    Admin - Header with user info

--- templates/admin/permissions.php
+++ templates/admin/permissions.php
@@ -19,7 +19,15 @@ require_once('templates/header.php');
             <a class="navbar-brand" href="/home"><i class="fa fa-home fa-fw fa-lg"></i>Grader Home</a>
         </div>
         <!-- /.navbar-header -->
-
+        <ul class="nav navbar-nav navbar-right" style="margin-right: 5%">
+            <?php echo '<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">' . Security::getLoggedInName() . '<b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                    <li><a href="/account"><span class="navspan">Account</span></a></li>
+                    <li><a href="/account/studentlists"><span class="navspan">Student lists</span></a></li>
+                    <li><a href="/admin/home"><span class="navspan">Admin Panel</span></a></li>
+                </ul>
+            </li>';
+            echo '<li><a href="#" data-bind="text: logoutBtn" id="logoutbtn" onClick="javascript: logoutUser();">Logout</a></li>'; ?>
         <ul class="nav navbar-nav navbar-right" style="margin-right: 5%">
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Language <b class="caret"></b></a>
