5545ee81fa522864df40185dd33e9ca5700240be
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-16 10:18:49 +0100

    Languages

--- js/adminIndexViewModel.js
+++ js/adminIndexViewModel.js
@@ -3,10 +3,10 @@ function pageViewModel(gvm) {
     gvm.title = ko.computed(function(){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("AdminPage");}, gvm);
     gvm.pageHeader = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("UserDashboardTitle");}, gvm);
 
-    gvm.changePermissions = ko.computed(function(){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("ChangePermissions");}, gvm);
+    gvm.changePermissions = ko.computed(function(){i18n.setLocale(gvm.lang()); i18n.__("ChangePermissions");}, gvm);
     gvm.changeUsers = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("ChangeUsers");}, gvm);
 
-    gvm.dashboard = ko.computed(function(){i18n.setLocale(gvm.lang()); return gvm.app() + ' - ' + i18n.__("AdminDashboard");}, gvm);
+    gvm.dashboard = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("AdminDashboard");}, gvm);
     gvm.rights = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("AdminRights");}, gvm);
     gvm.permissions = ko.computed(function(){i18n.setLocale(gvm.lang()); return i18n.__("AdminUsers");}, gvm);
 }
