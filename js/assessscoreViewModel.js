2a827283cbde5d9b1433e5c651eed138b5cdf150
nielsverhaegen <captustv@gmail.com>
2015-12-16 12:11:21 +0100

    Document Grading

--- js/assessscoreViewModel.js
+++ js/assessscoreViewModel.js
@@ -67,7 +67,7 @@ function fetchProjectScore()
 
 function saveProjectScore()
 {
-    console.log(viewModel.competences);
+    console.log(viewModel.competences());
     $.ajax({
         type: "POST",
         url: "/api/projectscore/" + projectid + "/" + studentid,
