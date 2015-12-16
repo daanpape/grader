76bba41a1b7183fbdd9cae5c1f77c55474e36e4c
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-16 22:37:43 +0100

    Beautify some buttons

--- templates/admin/edit.php
+++ templates/admin/edit.php
@@ -139,19 +139,17 @@ require_once('templates/header.php');
                         </tr>
                         </tbody>
                 </div>
+                <div class="row">
+                    <div class="col-lg-11">
+                    </div>
+                    <div class="col-lg-1">
+                        <button class="btn btn-default savePageBtn pagination-button pull-right" type="submit">
+                            <span class="glyphicon glyphicon-plus"></span>
+                            <span data-bind="text: saveBtn">Save</span>
+                        </button>
+                    </div>
+                </div>
             </form>
-
-
-            <div class="row">
-                <div class="col-lg-11">
-                </div>
-                <div class="col-lg-1">
-                    <button class="btn btn-default savePageBtn pagination-button pull-right" type="submit">
-                        <span class="glyphicon glyphicon-plus"></span>
-                        <span data-bind="text: saveBtn">Save</span>
-                    </button>
-                </div>
-            </div>
         </div>
     </div>
     <!-- /.row -->
