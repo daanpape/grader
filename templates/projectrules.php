f905cc848d1592c4bb607fcaa642683cf650149a
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-16 21:16:41 +0100

    Beautify some buttons

--- templates/projectrules.php
+++ templates/projectrules.php
@@ -60,7 +60,7 @@ $location = "projectRules";
                                 <select class="form-control form-next" style="display: inline-block; width: 25%" data-bind="foreach: viewModel.availableSigns, value: sign">
                                     <option data-bind="text: $data"></option>
                                 </select>
-                                <input type="text" class="form-control form-next" style="display: inline-block; width: 75%" placeholder="Percent"  data-bind="value: result">
+                                <input type="text" class="form-control form-next" style="display: inline-block; width: 70%" placeholder="Percent"  data-bind="value: result">
                                 <button class="btn btn-default form-next"  data-bind="click: removeThisRule, text: viewModel.deleteRuleName">
                                     Remove this rule
                                 </button>
