467b7f537ce3460a365ffe3cf756325748c37809
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-16 20:57:46 +0100

    Beautify some buttons

--- templates/assessscore.php
+++ templates/assessscore.php
@@ -168,8 +168,9 @@ $location = "assessscore";
 <div class="container">
     <div class="row">
         <div id="bottom-col" class="col-md-12">
-            <button type="button" class="btn btn-default pagination-button" data-bind="text: savePage">
-                Save
+            <button type="button" class="btn btn-default pagination-button">
+                <span class="glyphicon glyphicon-floppy-disk"></span>
+                <span data-bind="text: savePage">Save</span>
             </button>
         </div>
     </div>
