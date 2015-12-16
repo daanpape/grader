fdaa48445174008dcc1c9c5a15c5944ec7d636af
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-16 21:27:26 +0100

    Beautify some buttons

--- templates/projectrules.php
+++ templates/projectrules.php
@@ -57,10 +57,12 @@ $location = "projectRules";
                         <td>
                             <div>
                                 <span data-bind="text: viewModel.ruleTotalScore" style="display: inline-block">Total score:</span>
-                                <select class="form-control form-next" style="display: inline-block; width: 20%" data-bind="foreach: viewModel.availableSigns, value: sign">
+                                <select class="form-control form-next" style="display: inline-block; width: 30%" data-bind="foreach: viewModel.availableSigns, value: sign">
                                     <option data-bind="text: $data"></option>
                                 </select>
-                                <input type="text" class="form-control form-next" style="display: inline-block; width: 20%" placeholder="Percent"  data-bind="value: result">
+                                <input type="text" class="form-control form-next" style="display: inline-block; width: 30%" placeholder="Percent"  data-bind="value: result">
+                            </div>
+                            <div>
                                 <button class="btn btn-default form-next"  data-bind="click: removeThisRule, text: viewModel.deleteRuleName">
                                     Remove this rule
                                 </button>
