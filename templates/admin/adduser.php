fee714a9d4ca18cd0ec3ef62491eac76da4b5f65
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-16 22:20:46 +0100

    Beautify some buttons

--- templates/admin/adduser.php
+++ templates/admin/adduser.php
@@ -129,7 +129,7 @@ require_once('templates/header.php');
 
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-12">
+                    <div class="col-default-12">
                         <button class="btn btn-default savePageBtn pull-right" type="submit">
                             <span class="glyphicon glyphicon-floppy-disk"></span>
                             <span data-bind="text: saveBtn">Save</span>
