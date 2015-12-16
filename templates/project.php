b01f0f5e831eeb51c61b28e0587d90b75f9873e0
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-16 20:45:54 +0100

    Beautify some buttons

--- templates/project.php
+++ templates/project.php
@@ -94,7 +94,7 @@ $location = "project";
                                             <input type="text" placeholder="Description" class="form-control form-next" data-bind="value: description">
                                             <span data-bind="text: viewModel.projectWeight">Current weight:</span> <input type="text" class="form-control form-next" data-bind="value: weight">%
                                             <i class="icon-large icon-unlock" style="vertical-align: middle;" data-bind="click: toggleLock"></i>
-                                            <button class="btn pagination-button" data-bind="click: removeThis, text: viewModel.projectRemoveIndicator">Remove this indicator</button>
+                                            <button class="btn btn-default" data-bind="click: removeThis, text: viewModel.projectRemoveIndicator">Remove this indicator</button>
                                             <div class="dropdown">
                                                 <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown"><span data-bind="text: pointType"></span>
                                                     <span class="caret"></span></button>
@@ -106,15 +106,15 @@ $location = "project";
                                     </ul>
                                 </div>
                                 <div class="panel-footer color-subcomp">
-                                    <button class="btn pagination-button" value="NaN-0" data-bind="click: addIndicator, text: viewModel.projectAddIndicator">Add an indicator</button>
-                                    <button class="btn pagination-button pull-right" data-bind="click: removeThis, text: viewModel.projectRemoveSubComptence">Remove this subcompetence</button>
+                                    <button class="btn btn-default" value="NaN-0" data-bind="click: addIndicator, text: viewModel.projectAddIndicator">Add an indicator</button>
+                                    <button class="btn btn-default pull-right" data-bind="click: removeThis, text: viewModel.projectRemoveSubComptence">Remove this subcompetence</button>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="panel-footer">
-                        <button class="btn pagination-button" value="NaN" data-bind="click: addSubCompetence, text: viewModel.projectAddSubComptence">Add a subcompetence</button>
-                        <button class="btn pagination-button pull-right" value="NaN" data-bind="click: removeThis, text: viewModel.projectRemoveComptence">Remove this competence</button>
+                        <button class="btn btn-default" value="NaN" data-bind="click: addSubCompetence, text: viewModel.projectAddSubComptence">Add a subcompetence</button>
+                        <button class="btn btn-default pull-right" value="NaN" data-bind="click: removeThis, text: viewModel.projectRemoveComptence">Remove this competence</button>
                     </div>
                 </div>
             </div>
