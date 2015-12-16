ba6c6d370d5407082d698f307103a904eec58965
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-16 11:14:07 +0100

    Completed / Total

--- templates/assessproject.php
+++ templates/assessproject.php
@@ -50,6 +50,7 @@ $location = "assessproject";
         <div class="col-md-12">
             <table class="table table-striped">
                 <tr>
+                    <div>
                     <td><input type="text" id="searchField" class="form-control form-next" placeholder="Name of the student" data-bind="value: searchStudent"></td>
                     <td><a href="#" data-bind="click: function() { getStudentByName() }" type="button" class="btn btn-default">
                             <span class="glyphicon glyphicon-search"></span>
@@ -57,6 +58,7 @@ $location = "assessproject";
                             <span class=" glyphicon glyphicon-refresh"></span>
                         </a></td>
                     <td><p data-bind="text: totalCompleted"></p><p>/</p><p data-bind="text: totalStudents"></p></p></td>
+                    </div>
                 </tr>
             </table>
         </div>
