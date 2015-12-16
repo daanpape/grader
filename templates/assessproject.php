4eb35bfe66fb784e85f89965255488ac446e08bc
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-16 11:22:40 +0100

    Completed / Total

--- templates/assessproject.php
+++ templates/assessproject.php
@@ -56,7 +56,7 @@ $location = "assessproject";
                         </a> <a href="#" data-bind="click: function() { showFullList() }" type="button" class="btn btn-default">
                             <span class=" glyphicon glyphicon-refresh"></span>
                         </a></td>
-                    <td style="display:inline-block" class="btn btn-default"><p style="display:inline-block" data-bind="text: totalCompleted"></p><p style="display:inline-block; margin-left: 2px; margin-right: 2px">/</p><p style="display:inline-block" data-bind="text: totalStudents"></p></p></td>
+                    <td style="display:inline-block" class="btn btn-default"><p style="display:inline-block" data-bind="text: totalCompleted"></p><p style="display:inline-block; margin-left: 2px; margin-right: 2px">/</p><p style="display:inline-block" data-bind="text: totalStudents"></p></td>
                 </tr>
             </table>
         </div>
