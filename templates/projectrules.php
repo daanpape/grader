97caecc315e26d351e2f73d14f4528fab24f418d
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-16 21:39:23 +0100

    Beautify some buttons

--- templates/projectrules.php
+++ templates/projectrules.php
@@ -57,6 +57,8 @@ $location = "projectRules";
                         <td>
                             <div>
                                 <span data-bind="text: viewModel.ruleTotalScore">Total score:</span>
+                            </div>
+                            <div>
                                 <select class="form-control form-next" style="width: auto; display: inline-block" data-bind="foreach: viewModel.availableSigns, value: sign">
                                     <option data-bind="text: $data"></option>
                                 </select>
