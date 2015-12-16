7057a07661837c5e799f40e76648f8fd3d51531a
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-16 18:27:25 +0100

    English locales update

--- templates/menu.php
+++ templates/menu.php
@@ -47,7 +47,7 @@ require_once 'dptcms/config.php';
                 ?>
 
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-bind="text: menuLanguage">Language</a></b>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-bind="html: menuLanguage">Language</a></b>
                     <ul class="dropdown-menu">
                         <li><span class="navspan" onclick="setLang('en')">English</span></li>
                         <li><span class="navspan" onclick="setLang('nl')">Nederlands</span></li>
