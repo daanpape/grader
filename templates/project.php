1cdebba490536c63fe0ac11f09402e564b1d16c0
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-16 20:49:08 +0100

    Beautify some buttons

--- templates/project.php
+++ templates/project.php
@@ -51,11 +51,11 @@ $location = "project";
             <h1 class="page-header" id="projectHeader" data-value="<?php echo $projectid ?>" data-bind="text: pageHeader">Project</h1>
             <div class="row">
                 <div id="top-col" class="col-md-12">
-                    <button  type="button" class="btn btn-default  pull-right"  data-bind="text: addCompetenceBtn">
+                    <button  type="button" class="btn btn-default"  data-bind="text: addCompetenceBtn">
                         Add competence
                     </button>
 
-                    <button  type="button" class="btn btn-default "  data-bind="text: nextPage">
+                    <button  type="button" class="btn btn-default pull-right"  data-bind="text: nextPage">
                         Save
                     </button>
                 </div>
