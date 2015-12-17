f1b6fbe2a0e2abd14c3141d5da79406fab2571ab
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-17 14:26:54 +0100

    Merge Admin page to 1

--- templates/admin/edit.php
+++ templates/admin/edit.php
@@ -33,29 +33,9 @@ require_once('templates/header.php');
                 </ul>
             </li>
         </ul>
-
-        <!-- /.navbar-header -->
-
-        <div class="navbar-default sidebar" role="navigation">
-            <div class="sidebar-nav navbar-collapse">
-                <ul class="nav" id="side-menu">
-                    <li>
-                        <a href="/admin/home"><i class="fa fa-home fa-fw fa-lg"></i> Dashboard</a>
-
-                    <li>
-                        <a href="/admin/permissions"><i class="fa fa-wrench fa-fw fa-2x"></i> Rechten</span></a>
-                    </li>
-                    <li>
-                        <a href="/admin/users"><i class="fa fa-users fa-fw fa-lg"></i> Gebruikers</span></a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.sidebar-collapse -->
-        </div>
-        <!-- /.navbar-static-side -->
     </nav>
 
-    <div id="page-wrapper">
+    <div style="width: 90%; margin: 0 auto; margin-top: 2%">
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header" id="usereditHeader" data-value="<?php echo $edituserid ?>"  data-bind="text: pageHeaderEditUser">Edit User</h1>
