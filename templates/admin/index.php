69e917e27481ab094b4b4f6f106ec6a4a07489b3
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-17 13:41:12 +0100

    Merge Admin page to 1

--- templates/admin/index.php
+++ templates/admin/index.php
@@ -33,29 +33,9 @@ require_once('templates/header.php');
                 </ul>
             </li>
         </ul>
-
-        <!-- /.navbar-header -->
-
-<!--        <div class="navbar-default sidebar" role="navigation">-->
-<!--            <div class="sidebar-nav navbar-collapse">-->
-<!--                <ul class="nav" id="side-menu">-->
-<!--                    <li>-->
-<!--                        <a href="/admin/home" data-bind="text: dashboard">Dashboard</a>-->
-<!--                    </li>-->
-<!--                    <li>-->
-<!--                        <a href="/admin/permissions" data-bind="text: rights">Rechten</span></a>-->
-<!--                    </li>-->
-<!--                    <li>-->
-<!--                        <a href="/admin/users" data-bind="text: permissions"><i class="fa fa-users fa-fw fa-lg"></i> Gebruikers</span></a>-->
-<!--                    </li>-->
-<!--                </ul>-->
-<!--            </div>-->
-<!--            <!-- /.sidebar-collapse -->
-<!--        </div>-->
-        <!-- /.navbar-static-side -->
     </nav>
 
-    <div id="page-wrapper">
+    <div>
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header" data-bind="text: pageHeader">Dashboard</h1>
