77334e900460e8248899b18121312e4dfd61bb79
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-17 16:06:02 +0100

    small fixes

--- templates/admin/index.php
+++ templates/admin/index.php
@@ -63,7 +63,7 @@ require_once('templates/header.php');
                 </div>
             </div>
             <!-- /.col-lg-12 -->
-            <div class="row">
+            <div class="container">
                 <div class="table-responsive">
                     <table class="table table-striped">
                         <thead>
