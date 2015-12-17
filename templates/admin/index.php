9aca8589db972f37b97744fee0c4436b12cf72aa
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-17 14:54:20 +0100

    Admin page menu

--- templates/admin/index.php
+++ templates/admin/index.php
@@ -6,30 +6,31 @@ require_once('templates/header.php');
 <body style="padding-top: 0px">
 <div id="wrapper">
     <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand" href="/home" data-bind="text: projectname">Grader</a>
-                <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav">
-                        <li><a href="/admin/home">Admin Home</a></li>
-                    </ul>
-                </div>
             </div>
-
-            <ul class="nav navbar-nav navbar-right" style="margin-right: 5%">
-                <?php echo '<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">' . Security::getLoggedInName() . '<b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                    <li><a href="/account"><span class="navspan">Account</span></a></li>
-                    <li><a href="/account/studentlists"><span class="navspan">Student lists</span></a></li>
-                    <li><a href="/admin/home"><span class="navspan">Admin Panel</span></a></li>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="/admin/home">Admin Home</a></li>
                 </ul>
+            </div>
+        </div>
+
+        <ul class="nav navbar-nav navbar-right" style="margin-right: 5%">
+            <?php echo '<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">' . Security::getLoggedInName() . '<b class="caret"></b></a>
+            <ul class="dropdown-menu">
+                <li><a href="/account"><span class="navspan">Account</span></a></li>
+                <li><a href="/account/studentlists"><span class="navspan">Student lists</span></a></li>
+                <li><a href="/admin/home"><span class="navspan">Admin Panel</span></a></li>
+            </ul>
             </li>'; ?>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-bind="html: menuLanguage">Language</a></b>
