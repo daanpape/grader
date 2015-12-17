94b1e882704aa6be19774ac52b07dfb9d8de6726
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-17 14:45:48 +0100

    Reroute edit page

--- templates/admin/index.php
+++ templates/admin/index.php
@@ -15,6 +15,7 @@ require_once('templates/header.php');
                 <span class="icon-bar"></span>
             </button>
             <a class="navbar-brand" href="/home"><i class="fa fa-home fa-fw fa-lg"></i>Grader Home</a>
+            <a class="navbar-brand" href="/admin/home"><i class="fa fa-home fa-fw fa-lg"></i>Admin Home</a>
         </div>
 
         <ul class="nav navbar-nav navbar-right" style="margin-right: 5%">
