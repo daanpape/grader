e0692a0ac451e99f22c819b48c097140f970a8ae
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-17 14:58:46 +0100

    Admin page menu

--- templates/admin/index.php
+++ templates/admin/index.php
@@ -4,53 +4,53 @@ $location = "adminIndex";
 require_once('templates/header.php');
 ?>
 <body style="padding-top: 0px">
-<div id="wrapper">
     <!-- Navigation -->
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="/home" data-bind="text: projectname">Grader</a>
-            </div>
-            <div class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li><a href="/admin/home">Admin Home</a></li>
-                </ul>
-            </div>
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/home" data-bind="text: projectname">Grader</a>
         </div>
-
-
-        <ul class="nav navbar-nav navbar-right">
-
-            <?php
-            if (Security::isUserLoggedIn()) {
-
-                echo '<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">' . Security::getLoggedInName() . '<b class="caret"></b></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="/account"><span class="navspan">Account</span></a></li>
-                                <li><a href="/account/studentlists"><span class="navspan">Student lists</span></a></li>
-                                 <li><a href="/admin/home"><span class="navspan">Admin Panel</span></a></li>
-                            </ul>
-                        </li>';
-            } else {
-            }
-            ?>
-
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-bind="html: menuLanguage">Language</a></b>
-                <ul class="dropdown-menu">
-                    <li><span class="navspan" onclick="setLang('en')">English</span></li>
-                    <li><span class="navspan" onclick="setLang('nl')">Nederlands</span></li>
-                </ul>
-            </li>
-        </ul>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="/admin/home">Admin Home</a></li>
+            </ul>
+        </div>
     </div>
 
+
+    <ul class="nav navbar-nav navbar-right">
+
+        <?php
+        if (Security::isUserLoggedIn()) {
+
+            echo '<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">' . Security::getLoggedInName() . '<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="/account"><span class="navspan">Account</span></a></li>
+                            <li><a href="/account/studentlists"><span class="navspan">Student lists</span></a></li>
+                             <li><a href="/admin/home"><span class="navspan">Admin Panel</span></a></li>
+                        </ul>
+                    </li>';
+        } else {
+        }
+        ?>
+
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-bind="html: menuLanguage">Language</a></b>
+            <ul class="dropdown-menu">
+                <li><span class="navspan" onclick="setLang('en')">English</span></li>
+                <li><span class="navspan" onclick="setLang('nl')">Nederlands</span></li>
+            </ul>
+        </li>
+    </ul>
+</div>
+
+<div id="wrapper">
     <div style="width: 90%; margin: 0 auto; margin-top: 2%">
         <div class="row">
             <div class="col-lg-12">
