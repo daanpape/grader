6c9e2cab39b5054266783b4ca1e562e23e5cbcf1
jonasvanalderweireldt <vanal.jonas@gmail.com>
2015-12-17 13:41:36 +0100

    Merge Admin page to 1

--- templates/admin/index.php
+++ templates/admin/index.php
@@ -35,7 +35,7 @@ require_once('templates/header.php');
         </ul>
     </nav>
 
-    <div>
+    <div id="page-wrapper">
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header" data-bind="text: pageHeader">Dashboard</h1>
