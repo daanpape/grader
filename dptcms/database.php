a91ac201121bfa7fe5f97c0f1be4b697a164b467
Niels Verhaegen <captustv@gmail.com>
2015-12-28 16:34:53 +0100

    Fixing save grades

--- dptcms/database.php
+++ dptcms/database.php
@@ -981,8 +981,8 @@ class ClassDAO
             foreach ($data as $competences) {
                 foreach ($competences->subcompetences as $subcompetences) {
                     foreach ($subcompetences->indicators as $indicators) {
-                        error_log(($indicators->scoreid != 0),0);
-                        if ($indicators->scoreid != 0) {
+                        error_log((string)$indicators->scoreid,0);
+                        if ((string)$indicators->scoreid != '0') {
                             $stmt = $conn->prepare("INSERT INTO assess_score (project, student, user, competence, subcompetence, indicator, score ) VALUES (?,?,?,?,?,?,?)");
                             $stmt->execute(array($projectid, $studentid, $userid, $competences->id, $subcompetences->id, $indicators->id, $indicators->score));
                         } else {
