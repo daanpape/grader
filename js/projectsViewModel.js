fd324e2289ee39b5af8fcfe24a952d2d41195d3e
Niels Verhaegen <captustv@gmail.com>
2015-12-29 15:55:10 +0100

    Grade documents

--- js/projectsViewModel.js
+++ js/projectsViewModel.js
@@ -370,8 +370,8 @@ function showNewProjectTypeModal()
                 <input type="text" class="form-control input-lg" placeholder="'+ i18n.__("AddNewProjectTypePlaceholder") + '" name="nrOfAssessing"> \
             </div> \
             <div class="form-group"> \
-                <input type="text" style="display:inline-block" class="form-control input-lg" placeholder="' + i18n.__("DocumentPercentTitle") + '" name="documentProcent"> \
-                <input type="text" style="display:inline-block" class="form-control input-lg" placeholder="' + i18n.__("ProjectPercentTitle") + '" name="projectProcent"> \
+                <input type="text" style="display:inline-block; width: 49%; margin-right: 1%;" class="form-control input-lg" placeholder="' + i18n.__("DocumentPercentTitle") + '" name="documentProcent"> \
+                <input type="text" style="display:inline-block; width: 49%;" class="form-control input-lg" placeholder="' + i18n.__("ProjectPercentTitle") + '" name="projectProcent"> \
             </div> \
         </form>');
 
