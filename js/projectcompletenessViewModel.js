b31eaed9d2d63b57421ada0dfe7c7e2ef5b58c72
Niels Verhaegen <captustv@gmail.com>
2016-01-02 23:49:34 +0100

    Grade documents

--- js/projectcompletenessViewModel.js
+++ js/projectcompletenessViewModel.js
@@ -119,4 +119,12 @@ var saveDocuments = function() {
 function initPage() {
     viewModel.getProjectInfo();
     viewModel.getDocuments();
+
+    $('.table-responsive').on('show.bs.dropdown', function () {
+        $('.table-responsive').css( "overflow", "inherit" );
+    });
+
+    $('.table-responsive').on('hide.bs.dropdown', function () {
+        $('.table-responsive').css( "overflow", "auto" );
+    })
 }
