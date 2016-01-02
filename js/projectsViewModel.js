cce76165817cb6d328600a267e1afe546e88afa6
Niels Verhaegen <captustv@gmail.com>
2016-01-02 18:07:30 +0100

    Grade documents

--- js/projectsViewModel.js
+++ js/projectsViewModel.js
@@ -397,7 +397,7 @@ function showEditProjectTypeModal(code, name, description, tid, nrOfAssessing, p
     setGeneralModalTitle(i18n.__("EditProjectTitle"));
     setGeneralModalBody(createFormModal('updateprojectform', code, name, description, tid, nrOfAssessing, projectPercent, documentPercent));
 
-    $('#updateprojectform').validator().on('submit', function (e) {
+    $('#updateprojectform').validator().submit(function (e) {
         if (e.isDefaultPrevented()) {
             console.log('Test');
         } else {
@@ -420,7 +420,7 @@ function showEditProjectTypeModal(code, name, description, tid, nrOfAssessing, p
 }
 
 function createFormModal(idname, code, name, description, tid, nrOfAssessing, projectPercent, documentPercent) {
-    var form = '<form id="#' + idname  + '"> \
+    var form = '<form id="#' + idname  + '" METHOD="post"> \
             <div class="form-group"> \
                 <input type="text" class="form-control input-lg" placeholder="' + i18n.__('CodeTableTitle') + '" " name="code" value="' + code + '" required> \
                 <div class="help-block with-errors"></div>\
