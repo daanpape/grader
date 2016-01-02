d6ba68a5428730390d23d6fd27b5d7b0d5887c76
Niels Verhaegen <captustv@gmail.com>
2016-01-02 18:10:47 +0100

    Grade documents

--- js/projectsViewModel.js
+++ js/projectsViewModel.js
@@ -395,7 +395,36 @@ function showEditProjectTypeModal(code, name, description, tid, nrOfAssessing, p
 {
    resetGeneralModal();
     setGeneralModalTitle(i18n.__("EditProjectTitle"));
-    setGeneralModalBody(createFormModal('updateprojectform', code, name, description, tid, nrOfAssessing, projectPercent, documentPercent));
+    setGeneralModalBody('<form id="#updateprojectform"> \
+            <div class="form-group"> \
+                <input type="text" class="form-control input-lg" placeholder="' + i18n.__('CodeTableTitle') + '" " name="code" value="' + code + '" required> \
+                <div class="help-block with-errors"></div>\
+            </div> \
+            <div class="form-group"> \
+                <input type="text" class="form-control input-lg" placeholder="' + i18n.__('NameTableTitle') + '" name="name" value="' + name + '" required> \
+                    <div class="help-block with-errors"></div> \
+            </div> \
+            <div class="form-group"> \
+                <input type="text" class="form-control input-lg" placeholder="' + i18n.__('DescTableTitle') + '" name="description" value="' + description + '" required> \
+                    <div class="help-block with-errors"></div> \
+            </div> \
+            <div class="form-group"> \
+                <input required type="number" class="form-control input-lg" placeholder="'+ i18n.__("AddNewProjectTypePlaceholder") + '" name="nrOfAssessing" value="' + nrOfAssessing + '" > \
+                    <div class="help-block with-errors"></div> \
+            </div> \
+            <div class="form-group"> \
+                <div style="display:inline-block; width: 49%; margin-right: 1%;" >\
+                    <input type="number" id="doc" class="auto-complete form-control input-lg" placeholder="' + i18n.__("DocumentPercentTitle") + '" name="documentPercent" value="'+ documentPercent + '" required> \
+                    <div class="help-block with-errors"></div>\
+                </div>\
+                <div style="display:inline-block; width: 49%;" >\
+                    <input type="number" id="proj" class="auto-complete form-control input-lg" placeholder="' + i18n.__("ProjectPercentTitle") + '" name="projectPercent" value="' + projectPercent + '" required> \
+                    <div class="help-block with-errors"></div>\
+                </div>\
+            </div> \
+            <input type="submit" class="btn btn-default" value="' + i18n.__("SaveBtn") + '" > \
+            <input type="button"  class="btn btn-default" id="cancelBtn" value="' + i18n.__("CancelBtn") + '" > \
+        </form>');
 
     $('#updateprojectform').validator().on('submit', function (e) {
         if (e.isDefaultPrevented()) {
