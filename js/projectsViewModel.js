766289e85c8262686d299018e999e79cea90bf5d
Niels Verhaegen <captustv@gmail.com>
2016-01-02 20:15:31 +0100

    Grade documents

--- js/projectsViewModel.js
+++ js/projectsViewModel.js
@@ -401,7 +401,7 @@ function showEditProjectTypeModal(code, name, description, tid, nrOfAssessing, p
         if (e.isDefaultPrevented()) {
         } else {
             e.preventDefault();
-            console.log(Math.abs(parseInt($("#proj").val())));
+            console.log(Math.abs(parseInt($("#doc").val())) + Math.abs(parseInt($("#proj").val())));
             if(projectPercent + documentPercent == 100) {
                 updateProjecttypeForm(tid, $('#updateprojectform').serialize(), function (result) {
                     hideModal();
