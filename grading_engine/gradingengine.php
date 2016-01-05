1594c8630c5aa3ed2ad32e512e8dfcd3b0b0def1
Niels Verhaegen <captustv@gmail.com>
2016-01-05 11:28:56 +0100

    Grade documents

--- grading_engine/gradingengine.php
+++ grading_engine/gradingengine.php
@@ -338,7 +338,7 @@ class GradingEngine {
         $finalScoreProject->description = "Final score";
 
         $projectStructure[0] = $finalScoreProject;
-        $projectStructure[1] = GraderAPI::createAllDocuments($allDocuments);
+        $projectStructure[1] = GradingEngine::createAllDocuments($allDocuments);
 
         return $projectStructure[1];
         //return GradingEngine::calculateFinalScoreWithDocuments($documents,$allDocuments);
