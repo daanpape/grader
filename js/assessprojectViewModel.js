f1d2071a725e07ed45bd8a4bfdde84a7de7ab98e
Niels Verhaegen <captustv@gmail.com>
2016-01-05 11:40:24 +0100

    Grade documents

--- js/assessprojectViewModel.js
+++ js/assessprojectViewModel.js
@@ -107,7 +107,7 @@ function createPDF(id,name,lastname,email, projectheader, projectdescription)
         type: "GET",
         dataType: 'json',
         success: function(data) {
-            makePDF(id,name,lastname,email,projectheader,projectdescription,data);
+            //makePDF(id,name,lastname,email,projectheader,projectdescription,data);
             console.log(data);
         },
         error: function()
